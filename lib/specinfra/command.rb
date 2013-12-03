7385d2dc8dd15496c98c5301e7d02589199b12b7
Gosuke Miyashita <gosukenator@gmail.com>
2013-12-03 23:18:19 +0900

    Move Command::Solaris* and Command::SmartOS from serverspec

--- lib/specinfra/command.rb
+++ lib/specinfra/command.rb
@@ -7,6 +7,12 @@ require "specinfra/command/gentoo"
 require "specinfra/command/plamo"
 require "specinfra/command/redhat"
 
+# Solaris
+require "specinfra/command/solaris"
+require "specinfra/command/solaris10"
+require "specinfra/command/solaris11"
+require "specinfra/command/smartos"
+
 # Others
 require "specinfra/command/aix"
 require "specinfra/command/darwin"
