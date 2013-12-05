0af0a5c8c81bd0c7a50a3d4c25361b31460ec933
Gosuke Miyashita <gosukenator@gmail.com>
2013-12-05 15:28:28 +0900

    Override SpecInfra::Backend::Ssh#run_command for run nothing with tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,15 @@
 require 'specinfra'
 
 include SpecInfra::Helper::Exec
+
+module SpecInfra
+  module Backend
+    class Ssh
+      def run_command(cmd, opts={})
+        { :stdout => nil, :stderr => nil,
+          :exit_status => 0, :exit_signal => nil }
+      end
+    end
+  end
+end
+      
