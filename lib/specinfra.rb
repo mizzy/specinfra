1fcc5e813a141dce81a7879d4bac533b19a24ab6
Gosuke Miyashita <gosukenator@gmail.com>
2013-12-06 23:46:56 +0900

    Change behavior to call backend.set_example only if respont_to? :backend and :set_example to avoid occuring error when they are not defined

--- lib/specinfra.rb
+++ lib/specinfra.rb
@@ -11,21 +11,21 @@ module SpecInfra
     def configuration
       SpecInfra::Configuration
     end
+  end
+end
 
-    def configure
-      if defined?(RSpec)
-        RSpec.configure do |c|
-          c.include(SpecInfra::Helper::Configuration)
-          c.add_setting :os,            :default => nil
-          c.add_setting :host,          :default => nil
-          c.add_setting :ssh,           :default => nil
-          c.add_setting :sudo_password, :default => nil
-          c.add_setting :winrm,         :default => nil
-          SpecInfra.configuration.defaults.each { |k, v| c.add_setting k, :default => v }
-          c.before :each do
-            backend.set_example(example)
-          end
-        end
+if defined?(RSpec)
+  RSpec.configure do |c|
+    c.include(SpecInfra::Helper::Configuration)
+    c.add_setting :os,            :default => nil
+    c.add_setting :host,          :default => nil
+    c.add_setting :ssh,           :default => nil
+    c.add_setting :sudo_password, :default => nil
+    c.add_setting :winrm,         :default => nil
+    SpecInfra.configuration.defaults.each { |k, v| c.add_setting k, :default => v }
+    c.before :each do
+      if respond_to?(:backend) && backend.respond_to?(:set_example)
+        backend.set_example(example)
       end
     end
   end
