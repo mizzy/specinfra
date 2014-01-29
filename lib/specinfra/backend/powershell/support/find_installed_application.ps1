7e40e0fa202a1e71240cd2dfcf5d6823d20ac044
Edmund Dipple <elmundio1987@gmail.com>
2014-01-29 12:55:05 +0000

    cast result as array (allows counting length of 1)

--- lib/specinfra/backend/powershell/support/find_installed_application.ps1
+++ lib/specinfra/backend/powershell/support/find_installed_application.ps1
@@ -12,10 +12,10 @@ function FindInstalledApplication
   }   
 
   if ($appVersion -eq $null) { 
-    ($keys | Where-Object {$_.DisplayName -like $appName -or $_.PSChildName -like $appName}).Length -gt 0
+    @($keys | Where-Object {$_.DisplayName -like $appName -or $_.PSChildName -like $appName}).Length -gt 0
   }
   else{
-    ($keys | Where-Object {$_.DisplayName -like $appName -or $_.PSChildName -like $appName  } | Where-Object {$_.DisplayVersion -eq $appVersion} ).Length -gt 0
+    @($keys | Where-Object {$_.DisplayName -like $appName -or $_.PSChildName -like $appName  } | Where-Object {$_.DisplayVersion -eq $appVersion} ).Length -gt 0
   }
 
 }
