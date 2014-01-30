f0f71bc76ecaa186db155c96537716dc31e72c6e
Edmund Dipple <elmundio1987@gmail.com>
2014-01-30 16:13:09 +0000

    implement check_resolvable and check_reachable on windows platform

--- lib/specinfra/command/windows.rb
+++ lib/specinfra/command/windows.rb
@@ -181,6 +181,28 @@ module SpecInfra
         Backend::PowerShell::Command.new { exec cmd }
       end
 
+      def check_resolvable(name, type)
+        if type == "hosts"
+            cmd = "@(Select-String -path (Join-Path -Path $($env:windir) -ChildPath 'system32/drivers/etc/hosts') -pattern '#{name}\\b').count -gt 0"
+        else
+            cmd = "@([System.Net.Dns]::GetHostAddresses('#{name}')).count -gt 0"
+        end
+        Backend::PowerShell::Command.new { exec cmd }
+      end
+
+      def check_reachable(host, port, proto, timeout)
+          if port.nil?
+             Backend::PowerShell::Command.new do
+              exec "(New-Object System.Net.NetworkInformation.Ping).send('#{host}').Status -eq 'Success'"
+             end
+          else  
+            Backend::PowerShell::Command.new do
+             using 'is_remote_port_listening.ps1'
+             exec"(IsRemotePortListening -hostname #{host} -port #{port} -timeout #{timeout} -proto #{proto}) -eq $true"
+            end
+          end
+      end
+
       private
 
       def item_has_attribute item, attribute
