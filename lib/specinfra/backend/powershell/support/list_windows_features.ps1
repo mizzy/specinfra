bae2538d49adcb3f60872815ffc71071203f5c16
Edmund Dipple <elmundio1987@gmail.com>
2014-02-04 10:50:30 +0000

    Use dism.exe OR the Get-WindowsFeature cmdlet

--- lib/specinfra/backend/powershell/support/list_windows_features.ps1
+++ lib/specinfra/backend/powershell/support/list_windows_features.ps1
@@ -1,26 +1,45 @@
 function ListWindowsFeatures
 {    
-    $cachepath =  "${env:temp}/ListWindowsFeatures.xml"
+
+param(
+        [string]$feature,
+        [string]$provider="dism"
+         )
+
+    $cachepath =  "${env:temp}/ListWindowsFeatures-${provider}.xml"
     $maxAge = 2
 
     $cache =  Get-Item $cachepath -erroraction SilentlyContinue
 
     if($cache -ne $null -and ((get-date) - $cache.LastWriteTime).minutes -lt $maxage){
-        return Import-Clixml $cachepath
+        $features = Import-Clixml $cachepath |  Select *| Where-Object {(($_.name -like $feature) -or ($_.displayName -like $feature)) -and (($_.installed -eq $true) -or ($_.state -eq "Enabled"))}
+        return $features
     }
     else{
-        try
+
+        switch($provider)
         {
-            $dism = DISM /Online /Get-Features /Format:List | Where-Object {$_}     
+            "dism" { return features_dism | Select * | Where-Object {($_.name -eq $feature) -and ($_.state -eq "Enabled")}  }
+            "powershell" { return features_powershell | Select * | Where-Object {(($_.name -like $feature) -or ($_.displayName -like $feature)) -and ($_.installed -eq $true)} }
+            default {throw "Unsupported provider"}
+        }
+
+    }
+}
+
+function features_dism{
+      try
+        {
+            $out = DISM /Online /Get-Features /Format:List | Where-Object {$_}     
 
             if($LASTEXITCODE -ne 0)
             {
-                Write-Error $dism
+                Write-Error $out
                 Break
             }
 
-            $f = $dism[4..($dism.length-2)]
-            $feature = for($i=0; $i -lt $f.length;$i+=2)
+            $f = $out[4..($out.length-2)]
+            $features = for($i=0; $i -lt $f.length;$i+=2)
             {
                 $tmp = $f[$i],$f[$i+1] -replace '^([^:]+:\s)'
                 
@@ -30,14 +49,19 @@ function ListWindowsFeatures
                 }
             }
 
-            $feature | Export-Clixml $cachepath
+            $features | Export-Clixml $cachepath
 
-            return $feature
+            return $features
         }
         catch
         {
             Throw
         }
+}
 
-    }
+function features_powershell{
+     import-module servermanager
+     $features = Get-WindowsFeature
+     $features | Export-Clixml $cachepath
+     return Get-WindowsFeature 
 }
