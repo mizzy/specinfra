f3463301f0514c5a3a0a5863629826cb262ad888
Zvi "Viz" Effron <viz@flippedperspective.com>
2014-03-18 13:01:52 -0700

    Support SysV for Fedora 14 and under and share common functions with RedHat

--- lib/specinfra/command/fedora.rb
+++ lib/specinfra/command/fedora.rb
@@ -1,50 +1,28 @@
 module SpecInfra
   module Command
-    class Fedora < Linux
-      def check_access_by_user(file, user, access)
-        # Redhat-specific
-        "runuser -s /bin/sh -c \"test -#{access} #{file}\" #{user}"
-      end
-
+    class Fedora < RedHat
       def check_enabled(service, target="multi-user.target")
-        # Fedora-specific
-        # Fedora uses systemd which no longer has runlevels but targets
-        # For backwards compatibility, Fedora provides pseudo targets for
-        # runlevels
-        if target.is_a? Integer
-          target = "runlevel#{target}.target"
+        host = SpecInfra.configuration.ssh ? SpecInfra.configuration.ssh.host : 'localhost'
+        if property.has_key?(:os_by_host) && property[:os_by_host][host][:release].to_i < 15
+          super
+        else
+          # Fedora 15+ uses systemd which no longer has runlevels but targets
+          # For backwards compatibility, Fedora provides pseudo targets for
+          # runlevels
+          if target.is_a? Integer
+            target = "runlevel#{target}.target"
+          end
+          "systemctl --plain list-dependencies #{target} | grep '^#{escape(service)}.service$'"
         end
-        "systemctl --plain list-dependencies #{target} | grep '^#{escape(service)}.service$'"
       end
-
-      def check_yumrepo(repository)
-        "yum repolist all -C | grep ^#{escape(repository)}"
-      end
-
-      def check_yumrepo_enabled(repository)
-        "yum repolist all -C | grep ^#{escape(repository)} | grep enabled"
-      end
-
-      def check_installed(package,version=nil)
-        cmd = "rpm -q #{escape(package)}"
-        if version
-          cmd = "#{cmd} | grep -w -- #{escape(version)}"
-        end
-        cmd
-      end
-
-      alias :check_installed_by_rpm :check_installed
 
       def check_running(service)
-        "systemctl is-active #{escape(service)}.service"
-      end
-
-      def install(package)
-        cmd = "yum -y install #{package}"
-      end
-
-      def get_package_version(package, opts=nil)
-        "rpm -qi #{package} | grep Version | awk '{print $3}'"
+        host = SpecInfra.configuration.ssh ? SpecInfra.configuration.ssh.host : 'localhost'
+        if property.has_key?(:os_by_host) && property[:os_by_host][host][:release].to_i < 15
+          super
+        else
+          "systemctl is-active #{escape(service)}.service"
+        end
       end
     end
   end
