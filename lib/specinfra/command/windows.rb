344989711e88cf2e552e79c39a25519a1c0dfc64
Ross Niemi <dev@lone-cyprus.com>
2014-04-22 15:50:46 -0500

    Added support for looking up installed windows hot fixes

--- lib/specinfra/command/windows.rb
+++ lib/specinfra/command/windows.rb
@@ -235,6 +235,22 @@ module SpecInfra
         end
       end
 
+      def check_windows_hot_fix_installed(description, hot_fix_id=nil)
+        hot_fix_id_match = /(KB\d+)/i.match(description)
+        hot_fix_id = hot_fix_id_match ? hot_fix_id_match[1] : description if hot_fix_id.nil?
+
+        args = [ 
+          '-description', "'#{description}'",
+          '-hotFixId', "'#{hot_fix_id}'"
+        ]
+ 
+        cmd = "(FindInstalledHotFix #{args.join(' ')})" 
+        Backend::PowerShell::Command.new do
+          using 'find_installed_hot_fix.ps1'
+          exec "#{cmd} -eq $true"
+        end
+      end
+
       def check_file_version(name,version)
         cmd = "((Get-Command '#{name}').FileVersionInfo.ProductVersion -eq '#{version}') -or ((Get-Command '#{name}').FileVersionInfo.FileVersion -eq '#{version}')"
         Backend::PowerShell::Command.new { exec cmd }
