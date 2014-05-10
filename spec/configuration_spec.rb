2b342f8fb9c5896b8fd58c14650ad08d006e8379
Gosuke Miyashita <gosukenator@gmail.com>
2014-05-10 22:41:52 +0900

    Change SpecInfra to Specinfa after merging master

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -8,7 +8,7 @@ describe RSpec.configuration.path do
   it { should eq Specinfra.configuration.path }
 end
 
-SpecInfra.configuration.os = 'foo'
-describe SpecInfra.configuration.os do
+Specinfra.configuration.os = 'foo'
+describe Specinfra.configuration.os do
   it { should eq 'foo' }
 end
