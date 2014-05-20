570ef08ef2e561525382af1309864b80d585e6d6
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-21 01:53:47 +0900

    add ppa check methods

--- lib/specinfra/command/ubuntu.rb
+++ lib/specinfra/command/ubuntu.rb
@@ -4,6 +4,14 @@ module SpecInfra
       def check_running(service)
         "service #{escape(service)} status && service #{escape(service)} status | grep 'running'"
       end
+
+      def check_ppa(package)
+        "find /etc/apt/ -name \*.list | xargs grep -o \"deb http://ppa.launchpad.net/#{package}\""
+      end
+
+      def check_ppa_enabled(package)
+        "find /etc/apt/ -name \*.list | xargs grep -o \"^deb http://ppa.launchpad.net/#{package}\""
+      end
     end
   end
 end
