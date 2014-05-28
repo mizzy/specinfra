edeb2f38a75dd17e050cfaf720ab2979ad1b8800
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-28 22:13:08 +0900

    convert ppa:username/reponame to apt_line_uri
    accept `ppa:launchpad-username/reponame` style with ppa checking methods

--- lib/specinfra/command/ubuntu.rb
+++ lib/specinfra/command/ubuntu.rb
@@ -6,11 +6,21 @@ module SpecInfra
       end
 
       def check_ppa(package)
-        "find /etc/apt/ -name \*.list | xargs grep -o \"deb http://ppa.launchpad.net/#{escape(package)}\""
+        repo_name = to_apt_line_uri(package)
+        "find /etc/apt/ -name \*.list | xargs grep -o \"deb http://ppa.launchpad.net/#{escape(repo_name)}\""
       end
 
       def check_ppa_enabled(package)
-        "find /etc/apt/ -name \*.list | xargs grep -o \"^deb http://ppa.launchpad.net/#{escape(package)}\""
+        repo_name = to_apt_line_uri(package)
+        "find /etc/apt/ -name \*.list | xargs grep -o \"^deb http://ppa.launchpad.net/#{escape(repo_name)}\""
+      end
+
+      private
+
+      def to_apt_line_uri(repo)
+        regexp = /^ppa:/
+        return repo.gsub("ppa:","") if repo =~ regexp
+        repo
       end
     end
   end
