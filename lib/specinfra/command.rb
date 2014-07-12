6adbc6e7eabb918a5dfb275165bd9f511385cd2e
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-13 00:16:44 +0900

    Remove command classes other than base, linux and redhat temporary

--- lib/specinfra/command.rb
+++ lib/specinfra/command.rb
@@ -1,29 +1,7 @@
-require "specinfra/command/base"
+module Specinfra::Command; end
+
+require 'specinfra/command/base'
 
 # Linux
-require "specinfra/command/linux"
-require "specinfra/command/arch"
-require "specinfra/command/debian"
-require "specinfra/command/ubuntu"
-require "specinfra/command/gentoo"
-require "specinfra/command/plamo"
-require "specinfra/command/redhat"
-require "specinfra/command/redhat7"
-require "specinfra/command/suse"
-require "specinfra/command/opensuse"
-require "specinfra/command/fedora"
-require "specinfra/command/nixos"
-
-# Solaris
-require "specinfra/command/solaris"
-require "specinfra/command/solaris10"
-require "specinfra/command/solaris11"
-require "specinfra/command/smartos"
-
-# Others
-require "specinfra/command/aix"
-require "specinfra/command/darwin"
-require "specinfra/command/freebsd"
-require "specinfra/command/freebsd10"
-require "specinfra/command/openbsd"
-require "specinfra/command/windows"
+require 'specinfra/command/linux'
+require 'specinfra/command/redhat'
