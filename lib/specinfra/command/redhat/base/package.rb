bafaca61a35482e1c728d95bf90d092430ef72e4
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-13 14:00:11 +0900

    Add check_installed method to redhat/base/package

--- lib/specinfra/command/redhat/base/package.rb
+++ lib/specinfra/command/redhat/base/package.rb
@@ -1,5 +1,11 @@
 class Specinfra::Command::Redhat::Base::Package < Specinfra::Command::Base::Package
-  
+  def check_installed(package, version=nil)
+    cmd = "rpm -q #{escape(package)}"
+    if version
+      cmd = "#{cmd} | grep -w -- #{escape(version)}"
+    end
+    cmd
+  end
 end
 
 
