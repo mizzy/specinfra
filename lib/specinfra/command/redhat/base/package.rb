d96121ac96faa5e6932faacc5aa3efe3313cc8ff
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-13 23:29:02 +0900

    Change check_installed to check_is_installed

--- lib/specinfra/command/redhat/base/package.rb
+++ lib/specinfra/command/redhat/base/package.rb
@@ -1,5 +1,5 @@
 class Specinfra::Command::Redhat::Base::Package < Specinfra::Command::Base::Package
-  def check_installed(package, version=nil)
+  def check_is_installed(package, version=nil)
     cmd = "rpm -q #{escape(package)}"
     if version
       cmd = "#{cmd} | grep -w -- #{escape(version)}"
