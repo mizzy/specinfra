28dd452423f5153c1f25a06f3974f11fd2495c6e
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-13 23:33:32 +0900

    Fix spec

--- spec/command/redhat/package_spec.rb
+++ spec/command/redhat/package_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 property[:os_by_host] = {}
 set :os, { :family => 'redhat' }
 
-describe  commands.check_package_installed('httpd') do
+describe  commands.check_package_is_installed('httpd') do
   it { should eq 'rpm -q httpd' }
 end
