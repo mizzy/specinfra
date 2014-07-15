202399b58fa744e704a60ea3f58cc3608c3406e2
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-15 23:57:34 +0900

    Include command/base/* here instead of command/base.rb

--- lib/specinfra/command.rb
+++ lib/specinfra/command.rb
@@ -1,6 +1,10 @@
 module Specinfra::Command; end
 
 require 'specinfra/command/base'
+require 'specinfra/command/base/cron'
+require 'specinfra/command/base/file'
+require 'specinfra/command/base/package'
+require 'specinfra/command/base/routing_table'
 
 # Linux
 require 'specinfra/command/linux'
