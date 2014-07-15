39c63dbefcd23403dd58455e4ad0e6f81804d3c3
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-16 00:23:43 +0900

    Fix command/base/group

--- lib/specinfra/command.rb
+++ lib/specinfra/command.rb
@@ -3,6 +3,7 @@ module Specinfra::Command; end
 require 'specinfra/command/base'
 require 'specinfra/command/base/cron'
 require 'specinfra/command/base/file'
+require 'specinfra/command/base/group'
 require 'specinfra/command/base/package'
 require 'specinfra/command/base/routing_table'
 
---
+++ lib/specinfra/command/base/group.rb
@@ -0,0 +1,10 @@
+class Specinfra::Command::Base::Group < Specinfra::Command::Base
+  def check_exists(group)
+    "getent group #{escape(group)}"
+  end
+
+  def check_has_gid(group, gid)
+    regexp = "^#{group}"
+    "getent group | grep -w -- #{escape(regexp)} | cut -f 3 -d ':' | grep -w -- #{escape(gid)}"
+  end
+end
