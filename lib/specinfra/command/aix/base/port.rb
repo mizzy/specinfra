1357c15a34aa03e279870cc5279642cde4a135b5
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-20 13:13:02 +0900

    Fix AIX port and service

---
+++ lib/specinfra/command/aix/base/port.rb
@@ -0,0 +1,6 @@
+class Specinfra::Command::Aix::Base::Port < Specinfra::Command::Base::Port
+  def check_is_listening(port, options={})
+    regexp = "*.#{port} "
+    "netstat -an -f inet | awk '{print $4}' | grep  -- #{regexp}"
+  end
+end
