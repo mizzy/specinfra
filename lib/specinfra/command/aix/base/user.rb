96d82b2dccea68ee702a631b5e3d2d9d608ae2f1
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-20 13:21:17 +0900

    Fix AIX user

---
+++ lib/specinfra/command/aix/base/user.rb
@@ -0,0 +1,13 @@
+class Specinfra::Command::Aix::Base::User < Specinfra::Command::Base::User
+  def check_is_belonging_to_group(user, group)
+    "lsuser -a groups #{escape(user)} | awk -F'=' '{print $2}'| sed -e 's/,/ /g' |grep -w  -- #{escape(group)}"
+  end
+
+  def check_has_login_shell(user, path_to_shell)
+    "lsuser -a shell #{escape(user)} |awk -F'=' '{print $2}' | grep -w -- #{escape(path_to_shell)}"
+  end
+
+  def check_has_home_directory(user, path_to_home)
+    "lsuser -a home #{escape(user)} | awk -F'=' '{print $2}' | grep -w -- #{escape(path_to_home)}"
+  end
+end
