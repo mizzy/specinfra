dab60890eca0817e56c84d59dd82ff68b8b929ac
Gosuke Miyashita <gosukenator@gmail.com>
2014-07-25 22:27:02 +0900

    Fix check_lxc_container_is_running to work with any versions of LXC
    See #155

--- lib/specinfra/command/linux/base/lxc_container.rb
+++ lib/specinfra/command/linux/base/lxc_container.rb
@@ -4,6 +4,6 @@ class Specinfra::Command::Linux::Base::LxcContainer < Specinfra::Command::Base::
   end
 
   def check_is_running(container)
-    "lxc-info -n #{escape(container)} -t RUNNING"
+    "lxc-info -n #{escape(container)} -s | grep -w RUNNING"
   end
 end
