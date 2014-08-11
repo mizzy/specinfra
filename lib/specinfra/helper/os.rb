b06f05e5269ec2b52085939e4e7bdbb93fb5ae4a
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-11 22:06:12 +0900

    Simplify OS cache mecahism
    Specinfra v2 handle one host per one process.
    So need not to handle OS cache by host.

--- lib/specinfra/helper/os.rb
+++ lib/specinfra/helper/os.rb
@@ -2,33 +2,14 @@ require 'specinfra/helper/detect_os'
 
 module Specinfra::Helper::Os
   def os
-    property[:os_by_host] = {} if ! property[:os_by_host]
-    host_port = current_host_and_port
-
-    if property[:os_by_host][host_port]
-      os_by_host = property[:os_by_host][host_port]
-    else
-      # Set command object explicitly to avoid `stack too deep`
-      os_by_host = detect_os
-      property[:os_by_host][host_port] = os_by_host
+    property[:os] = {} if ! property[:os]
+    if ! property[:os].include?(:family)
+      property[:os] = detect_os
     end
-    os_by_host
+    property[:os]
   end
 
   private
-
-  # put this in a module for better reuse
-  def current_host_and_port
-    if Specinfra.configuration.ssh
-      "#{Specinfra.configuration.ssh.host}:#{Specinfra.configuration.ssh.options[:port]}"
-    elsif Specinfra.configuration.ssh_options
-          
-      "#{Specinfra.configuration.host}:#{Specinfra.configuration.ssh_options[:port]}"
-    else
-      "#{Specinfra.configuration.host}:0"
-    end
-  end
-
   def detect_os
     return Specinfra.configuration.os if Specinfra.configuration.os
     Specinfra::Helper::DetectOs.subclasses.each do |c|
