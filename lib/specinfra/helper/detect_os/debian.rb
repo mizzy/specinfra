081082c31ec5908fdb2be44efd51ec4dee7fcc1a
Gosuke Miyashita <gosuke-miyashita@cookpad.com>
2014-08-22 16:49:14 +0900

    Fix debian detection routine

--- lib/specinfra/helper/detect_os/debian.rb
+++ lib/specinfra/helper/detect_os/debian.rb
@@ -1,17 +1,19 @@
 class Specinfra::Helper::DetectOs::Debian < Specinfra::Helper::DetectOs
   def self.detect
     if run_command('ls /etc/debian_version').success?
+      distro  = nil
+      release = nil
       lsb_release = run_command("lsb_release -ir")
       if lsb_release.success?
-        if lsb_release.stdout =~ /:/
-          distro = lsb_release.stdout.split("\n").first.split(':').last
-          release = lsb_release.stdout.split("\n").last.split(':').last.strip
+        lsb_release.stdout.each_line do |line|
+          distro  = line.split(':').last.strip if line =~ /^Distributor ID:/
+          release = line.split(':').last.strip if line =~ /^Release:/
         end
       else
         lsb_release = run_command("cat /etc/lsb-release")
         if lsb_release.success?
           lsb_release.stdout.each_line do |line|
-            distro = line.split('=').last if line =~ /^DISTRIB_ID=/
+            distro  = line.split('=').last.strip if line =~ /^DISTRIB_ID=/
             release = line.split('=').last.strip if line =~ /^DISTRIB_RELEASE=/
           end
         end
