71ede6f87368604fd66d952c93c180354f61918d
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-26 16:52:42 +0900

    Add user related command spec

---
+++ spec/command/base/user_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe get_command(:get_user_uid, 'foo') do
+  it { should eq 'id -u foo' }
+end
+
+describe get_command(:get_user_gid, 'foo') do
+  it { should eq 'id -g foo' }
+end
+
+describe get_command(:get_user_home_directory, 'foo') do
+  it { should eq "getent passwd foo | awk -F: '{ print $6 }'" }
+end
+
+describe get_command(:update_user_uid, 'foo', 100) do
+  it { should eq 'usermod -u 100 foo' }
+end
+
+describe get_command(:update_user_gid, 'foo', 100) do
+  it { should eq 'usermod -g 100 foo' }
+end
+
+describe get_command(:add_user, 'foo', :home_directory => '/home/foo') do
+  it { should eq 'useradd -d /home/foo foo' }
+end
+
+describe get_command(:update_user_encrypted_password, 'foo', 'xxxxxxxx') do
+  it { should eq 'echo foo:xxxxxxxx | chpasswd -e' }
+end
+
+describe get_command(:get_user_encrypted_password, 'foo') do
+  it { should eq "getent shadow foo | awk -F: '{ print $2 }'" }
+end
+
