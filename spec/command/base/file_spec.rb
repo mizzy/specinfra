bbf271acaed79a449262e02c8c79f46768eddbaf
Gosuke Miyashita <gosukenator@gmail.com>
2014-08-27 11:43:10 +0900

    Add specs

--- spec/command/base/file_spec.rb
+++ spec/command/base/file_spec.rb
@@ -2,52 +2,55 @@ require 'spec_helper'
 
 set :os, { :family => nil }
 
-describe 'File related commands'  do
-  after do
-    property[:os_by_host] = nil
-  end
-
-  context get_command(:check_file_is_directory, '/tmp') do
-    it { should eq 'test -d /tmp' }
-  end
-
-  context get_command(:change_file_mode, '/tmp', '0644') do
-    it { should eq 'chmod 0644 /tmp' }
-  end
-
-  context get_command(:change_file_owner, '/tmp', 'root') do
-    it { should eq 'chown root /tmp' }
-  end
-
-  context get_command(:change_file_owner, '/tmp', 'root', 'root') do
-    it { should eq 'chown root:root /tmp' }
-  end
-
-  context get_command(:change_file_group, '/tmp', 'root') do
-    it { should eq 'chgrp root /tmp' }
-  end
-
-  context get_command(:create_file_as_directory, '/tmp') do
-    it { should eq 'mkdir -p /tmp' }
-  end
-
-  context get_command(:get_file_owner_user, '/tmp') do
-    it { should eq 'stat -c %U /tmp' }
-  end
-
-  context get_command(:get_file_owner_group, '/tmp') do
-    it { should eq 'stat -c %G /tmp' }
-  end
-
-  context get_command(:move_file, '/src', '/dest') do
-    it { should eq 'mv /src /dest' }
-  end
-
-  context get_command(:link_file_to, '/link', '/target') do
-    it { should eq 'ln -s /target /link' }
-  end
-
-  context get_command(:remove_file, '/tmp') do
-    it { should eq 'rm -rf /tmp' }
-  end
+describe get_command(:check_file_is_directory, '/tmp') do
+  it { should eq 'test -d /tmp' }
 end
+
+describe get_command(:change_file_mode, '/tmp', '0644') do
+  it { should eq 'chmod 0644 /tmp' }
+end
+
+describe get_command(:change_file_owner, '/tmp', 'root') do
+  it { should eq 'chown root /tmp' }
+end
+
+describe get_command(:change_file_owner, '/tmp', 'root', 'root') do
+  it { should eq 'chown root:root /tmp' }
+end
+
+describe get_command(:change_file_group, '/tmp', 'root') do
+  it { should eq 'chgrp root /tmp' }
+end
+
+describe get_command(:create_file_as_directory, '/tmp') do
+  it { should eq 'mkdir -p /tmp' }
+end
+
+describe get_command(:get_file_owner_user, '/tmp') do
+  it { should eq 'stat -c %U /tmp' }
+end
+
+describe get_command(:get_file_owner_group, '/tmp') do
+  it { should eq 'stat -c %G /tmp' }
+end
+
+describe get_command(:move_file, '/src', '/dest') do
+  it { should eq 'mv /src /dest' }
+end
+
+describe get_command(:link_file_to, '/link', '/target') do
+  it { should eq 'ln -s /target /link' }
+end
+
+describe get_command(:remove_file, '/tmp') do
+  it { should eq 'rm -rf /tmp' }
+end
+
+describe get_command(:check_file_is_link, '/tmp') do
+  it { should eq 'test -L /tmp' }
+end
+
+describe get_command(:get_file_link_target, '/tmp') do
+  it { should eq 'readlink -f /tmp' }
+end
+
