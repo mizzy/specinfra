b92033deb43ad7a66a0766d90b64c1db4d97ddea
Gosuke Miyashita <gosukenator@gmail.com>
2014-09-04 11:07:44 +0900

    Forwardport #215

--- lib/specinfra/command/linux/base/interface.rb
+++ lib/specinfra/command/linux/base/interface.rb
@@ -14,6 +14,17 @@ class Specinfra::Command::Linux::Base::Interface < Specinfra::Command::Base::Int
       ip_address.gsub!(".", "\\.")
       "ip addr show #{interface} | grep 'inet #{ip_address}'"
     end
+
+    def check_has_ipv6_address(interface, ip_address)
+      ip_address = ip_address.dup
+      if ip_address =~ /\/\d+$/
+        ip_address << " "
+      else
+        ip_address << "/"
+      end
+      ip_address.downcase!
+      "ip addr show #{interface} | grep 'inet6 #{ip_address}'"
+    end
   end
 end
 
---
+++ spec/command/linux/interface_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+set :os, :family => 'linux'
+
+describe get_command(:check_interface_has_ipv6_address, 'eth0', '2001:0db8:bd05:01d2:288a:1fc0:0001:10ee') do
+  it { should eq "ip addr show eth0 | grep 'inet6 2001:0db8:bd05:01d2:288a:1fc0:0001:10ee/'" }
+end
