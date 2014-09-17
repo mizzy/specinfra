b27a2cdf2a206d2bef1d087305cc24c01118fa01
Gosuke Miyashita <gosukenator@gmail.com>
2014-09-17 10:16:26 +0900

    Fowardport #220

--- lib/specinfra/command/openbsd/base/service.rb
+++ lib/specinfra/command/openbsd/base/service.rb
@@ -1,11 +1,11 @@
 class Specinfra::Command::Openbsd::Base::Service < Specinfra::Command::Base::Service
   class << self
     def check_is_enabled(service, level=nil)
-      "egrep '(#{escape(service)}_flags=|^pkg_scripts=\"(.*)#{escape(service)}(.*)\")' /etc/rc.conf.local | grep -v \=NO"
+      "rcctl status #{escape(service)}"
     end
 
     def check_is_running(service)
-      "/etc/rc.d/#{escape(service)} status"
+      "rcctl check #{escape(service)}"
     end
   end
 end
