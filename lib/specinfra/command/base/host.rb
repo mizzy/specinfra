ceeb4bc8d9fa66a546dfb441be22c2a9d7aee397
Dale <daleki@gmail.com>
2015-01-14 13:19:41 -0800

    nslookup exit code workaround

--- lib/specinfra/command/base/host.rb
+++ lib/specinfra/command/base/host.rb
@@ -2,7 +2,7 @@ class Specinfra::Command::Base::Host < Specinfra::Command::Base
   class << self
     def check_is_resolvable(name, type)
       if type == "dns"
-        "nslookup -timeout=1 #{escape(name)}"
+        "lookup=$(nslookup -timeout=1 #{escape(name)} | grep -A1 'Name:' | grep Address | awk -F': ' '{print $2}'); if [ "$lookup" ]; then $(exit 0); else $(exit 1); fi"
       elsif type == "hosts"
         "grep -w -- #{escape(name)} /etc/hosts"
       else
