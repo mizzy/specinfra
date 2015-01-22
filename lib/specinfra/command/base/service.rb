eba00dc44f16cb0dcd654cdc473669f983cea3f1
Adrian Irving-Beer <wisq@wisq.net>
2015-01-22 16:11:21 -0500

    Add check_is_running_under_runit.

--- lib/specinfra/command/base/service.rb
+++ lib/specinfra/command/base/service.rb
@@ -16,6 +16,10 @@ class Specinfra::Command::Base::Service < Specinfra::Command::Base
       "svstat /service/#{escape(service)} | grep -E 'up \\(pid [0-9]+\\)'"
     end
 
+    def check_is_running_under_runit(service)
+      "sv status #{escape(service)} | grep -E '^run: '"
+    end
+
     def check_is_monitored_by_monit(service)
       "monit status"
     end
