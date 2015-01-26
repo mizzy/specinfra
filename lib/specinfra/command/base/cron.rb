167179235d9482ddc11d5c4a674650f64edcbed7
mikeda <baguzy@gmail.com>
2015-01-26 21:08:45 +0900

    Add ^ and $ to crontab regexp

--- lib/specinfra/command/base/cron.rb
+++ lib/specinfra/command/base/cron.rb
@@ -2,10 +2,11 @@ class Specinfra::Command::Base::Cron < Specinfra::Command::Base
   class << self
     def check_has_entry(user, entry)
       entry_escaped = entry.gsub(/\*/, '\\*').gsub(/\[/, '\\[').gsub(/\]/, '\\]')
+      grep_command = "grep -v '^[[:space:]]*#' | grep -- ^#{escape(entry_escaped)}$"
       if user.nil?
-        "crontab -l | grep -v '^[[:space:]]*#' -- | grep -- #{escape(entry_escaped)}"
+        "crontab -l | #{grep_command}"
       else
-        "crontab -u #{escape(user)} -l | grep -v '^[[:space:]]*#' | grep -- #{escape(entry_escaped)}"
+        "crontab -u #{escape(user)} -l | #{grep_command}"
       end
     end
   end
