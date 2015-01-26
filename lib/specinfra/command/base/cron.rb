91e7d05d307908ec5e53227fbb3a4c5c5485c2f6
mikeda <baguzy@gmail.com>
2015-01-26 16:32:10 +0900

    Fix regex for crontab

--- lib/specinfra/command/base/cron.rb
+++ lib/specinfra/command/base/cron.rb
@@ -3,9 +3,9 @@ class Specinfra::Command::Base::Cron < Specinfra::Command::Base
     def check_has_entry(user, entry)
       entry_escaped = entry.gsub(/\*/, '\\*').gsub(/\[/, '\\[').gsub(/\]/, '\\]')
       if user.nil?
-        "crontab -l | grep -v \"#\" -- | grep -- #{escape(entry_escaped)}"
+        "crontab -l | grep -v '^[[:space:]]*#' -- | grep -- #{escape(entry_escaped)}"
       else
-        "crontab -u #{escape(user)} -l | grep -v \"#\" | grep -- #{escape(entry_escaped)}"
+        "crontab -u #{escape(user)} -l | grep -v '^[[:space:]]*#' | grep -- #{escape(entry_escaped)}"
       end
     end
   end
