ab6fd5485029899b3dab094bee3d0828288576a5
Stanley Karunditu <stanleyk@cumulusnetworks.com>
2015-02-12 12:43:37 -0500

    fix typo where bond.rb is creating bridge class instead of bond class

--- lib/specinfra/command/linux/base/bond.rb
+++ lib/specinfra/command/linux/base/bond.rb
@@ -1,4 +1,4 @@
-class Specinfra::Command::Linux::Base::Bridge < Specinfra::Command::Base::Bridge
+class Specinfra::Command::Linux::Base::Bond < Specinfra::Command::Base::Bond
   class << self
     def check_exists(name)
       "ip link show #{name}"
