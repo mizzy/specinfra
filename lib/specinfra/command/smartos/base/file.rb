28fd30a150a63b6cc44dacc95a0eb3722842de2e
Christopher Horrell <christopher@joyent.com>
2015-03-05 14:25:05 -0500

    Use full path to digest
    This way we don't accidentally use the pkgsrc version

--- lib/specinfra/command/smartos/base/file.rb
+++ lib/specinfra/command/smartos/base/file.rb
@@ -1,11 +1,11 @@
 class Specinfra::Command::Smartos::Base::File < Specinfra::Command::Solaris::Base::File
   class << self
     def get_md5sum(file)
-      "digest -a md5 -v #{escape(file)} | cut -d '=' -f 2 |  cut -c 2-"
+      "/usr/bin/digest -a md5 -v #{escape(file)} | cut -d '=' -f 2 |  cut -c 2-"
     end
     
     def get_sha256sum(file)
-      "digest -a sha256 -v #{escape(file)} | cut -d '=' -f 2 |  cut -c 2-"
+      "/usr/bin/digest -a sha256 -v #{escape(file)} | cut -d '=' -f 2 |  cut -c 2-"
     end
   end
 end
