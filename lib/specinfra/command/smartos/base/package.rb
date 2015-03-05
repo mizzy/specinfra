86d26fa20a7d6c94b785638bf67fdc84ecfb9aae
Christopher Horrell <christopher@joyent.com>
2015-03-05 12:44:54 -0500

    Use pkg_info for check_is_installed and get_version
    Previous version assumed GNU grep is installed which may not always be
    the case

--- lib/specinfra/command/smartos/base/package.rb
+++ lib/specinfra/command/smartos/base/package.rb
@@ -1,7 +1,7 @@
 class Specinfra::Command::Smartos::Base::Package < Specinfra::Command::Solaris::Base::Package
   class << self
     def check_is_installed(package, version=nil)
-      cmd = "/opt/local/bin/pkgin list 2> /dev/null | grep -qw ^#{escape(package)}"
+      cmd = "pkg_info -qE #{escape(package)}"
       if version
         cmd = "#{cmd}-#{escape(version)}"
       end
@@ -9,7 +9,7 @@ class Specinfra::Command::Smartos::Base::Package < Specinfra::Command::Solaris::
     end
 
     def get_version(package, opts=nil)
-      "pkgin list | cut -f 1 -d ' ' | grep -E '^#{escape(package)}-([^-])+$' | grep -Eo '(\\.|\\w)+$'"
+      "pkg_info -E #{escape(package)} | awk -F '-' '{print $NF}'"
     end
   end
 end
