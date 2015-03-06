7f75dd72eb199a8a4124a69d3ea56362160ae17f
Gosuke Miyashita <gosukenator@gmail.com>
2015-03-06 18:17:25 +0900

    Fix to extend Ec2Metadata easily by external gem

--- lib/specinfra/ec2_metadata.rb
+++ lib/specinfra/ec2_metadata.rb
@@ -3,20 +3,73 @@ module Specinfra
   class Ec2Metadata
     def initialize
       @base_uri = 'http://169.254.169.254/latest/meta-data/'
+      @metadata = {}
     end
 
-    def get(path='')
+    def get
+      @metadata = get_metadata
+      self
+    end
+
+    def [](key)
+      if @metadata[key].nil?
+        begin
+          require "specinfra/ec2_metadata/#{key}"
+          inventory_class = Specinfra::Ec2Metadata.const_get(key.to_camel_case)
+          @metadata[key] = inventory_class.get
+        rescue LoadError
+          @metadata[key] = nil
+        rescue
+          @metadata[key] = nil
+        end
+      end
+
+      @metadata[key]
+    end
+
+    def empty?
+      @metadata.empty?
+    end
+
+    def each
+      keys.each do |k|
+        yield k, @metadata[k]
+      end
+    end
+
+    def each_key
+      keys.each do |k|
+        yield k
+      end
+    end
+
+    def each_value
+      keys.each do |k|
+        yield @metadata[k]
+      end
+    end
+
+    def keys
+      @metadata.keys
+    end
+
+    def inspect
+      @metadata
+    end
+
+    private
+    def get_metadata(path='')
       metadata = {}
 
       keys = Specinfra::Runner.run_command("curl #{@base_uri}#{path}").stdout.split("\n")
 
       keys.each do |key|
         if key =~ %r{/$}
-          metadata[key[0..-2]] = get(path + key)
+          metadata[key[0..-2]] = get_metadata(path + key)
         else
           if key =~ %r{=}
             key = key.split('=')[0] + '/'
-            metadata[key[0..-2]] = get(path + key)
+            metadata[key[0..-2]] = get_metadata(path + key)
           else
             ret = get_endpoint(path)
             metadata[key] = get_endpoint(path + key) if ret
