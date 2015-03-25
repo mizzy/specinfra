5cab7eb757d653eb7dd7b38e621ebbedb159be39
Ryota Arai <ryota.arai@gmail.com>
2015-03-25 11:48:21 +0900

    Do not include helper modules from specinfra/core.

--- lib/specinfra/helper.rb
+++ lib/specinfra/helper.rb
@@ -1,13 +1,6 @@
 require 'specinfra/helper/os'
-include Specinfra::Helper::Os
-
 require 'specinfra/helper/docker'
 require 'specinfra/helper/lxc'
-
 require 'specinfra/helper/configuration'
-
 require 'specinfra/helper/properties'
-include Specinfra::Helper::Properties
-
 require 'specinfra/helper/host_inventory'
-include Specinfra::Helper::HostInventory
