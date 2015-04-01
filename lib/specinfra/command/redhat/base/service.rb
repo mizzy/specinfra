2911a22d8de3e292eac254f08f0b7fa80de2d0cd
Nathan Sullivan <nathan@nightsys.net>
2015-04-01 21:12:50 +1000

    fix service resource for centos 5.x, missing /sbin in PATH by default and sudo not in use

--- lib/specinfra/command/redhat/base/service.rb
+++ lib/specinfra/command/redhat/base/service.rb
@@ -1,15 +1,27 @@
 class Specinfra::Command::Redhat::Base::Service < Specinfra::Command::Linux::Base::Service
   class << self
+    # CentOS 5.x does not include /sbin in $PATH for non-root users.
+    def chkconfig_path ()
+      rh_rel_file = '/etc/redhat-release'
+      "chkconfig" unless File.exists? rh_rel_file
+      # Extra redundant File.exists? required otherwise spec tests fail.
+      if File.exists? rh_rel_file and IO.read(rh_rel_file).chomp.match(/^CentOS release 5\./)
+        "/sbin/chkconfig"
+      else
+        "chkconfig"
+      end
+    end
+
     def check_is_enabled(service, level=3)
-      "chkconfig --list #{escape(service)} | grep #{level}:on"
+      "#{chkconfig_path} --list #{escape(service)} | grep #{level}:on"
     end
 
     def enable(service)
-      "chkconfig #{escape(service)} on"
+      "#{chkconfig_path} #{escape(service)} on"
     end
 
     def disable(service)
-      "chkconfig #{escape(service)} off"
+      "#{chkconfig_path} #{escape(service)} off"
     end
 
     def start(service)
