1acf3bbc6009a889766a55d1f407ee88af48f752
Jonathan Hartman <j@p4nt5.com>
2015-04-30 19:14:40 -0700

    Check HKEY_CURRENT_USER in addition to HKEY_LOCAL_MACHINE

--- lib/specinfra/backend/powershell/support/find_installed_application.ps1
+++ lib/specinfra/backend/powershell/support/find_installed_application.ps1
@@ -4,12 +4,27 @@ function FindInstalledApplication
     
   if ((Get-WmiObject win32_operatingsystem).OSArchitecture -notmatch '64')  
   { 
-      $keys= (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*') 
+    $keys= (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*')
+    $possible_path= 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    if (Test-Path $possible_path)
+    {
+      $keys+= (Get-ItemProperty $possible_path)
+    }
   }  
     else  
   { 
-      $keys = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*') 
-  }   
+    $keys = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*','HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*')
+    $possible_path= 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    if (Test-Path $possible_path)
+    {
+      $keys+= (Get-ItemProperty $possible_path)
+    }
+    $possible_path= 'HKCU:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*'
+    if (Test-Path $possible_path)
+    {
+      $keys+= (Get-ItemProperty $possible_path)
+    }
+  }
 
   if ($appVersion -eq $null) { 
     @($keys | Where-Object {$_.DisplayName -like $appName -or $_.PSChildName -like $appName}).Length -gt 0
