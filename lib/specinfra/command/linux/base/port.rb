282c1d872458da472448e447cf70b4e10ae33552
Matt <brimstone@the.narro.ws>
2015-05-25 23:37:20 -0400

    Fixing error in column numbering.

--- lib/specinfra/command/linux/base/port.rb
+++ lib/specinfra/command/linux/base/port.rb
@@ -3,13 +3,12 @@ class Specinfra::Command::Linux::Base::Port < Specinfra::Command::Base::Port
     def check_is_listening(port, options = {})
       options[:local_address] = '*' unless options[:local_address]
       laddr = "#{options[:local_address]}:#{port}"
-      awk = "awk 'BEGIN {c=0} $6 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
       if options[:protocol] == 'tcp'
-        "ss -tnl | #{awk}"
+        "ss -tnl | awk 'BEGIN {c=0} $4 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
       elsif options[:protocol] == 'udp'
-        "ss -unl | #{awk}"
+        "ss -unl | awk 'BEGIN {c=0} $4 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
       else
-        "ss -nl | #{awk}"
+        "ss -nl | awk 'BEGIN {c=0} $5 == \"#{laddr}\" {c++} END { if (c>0) {exit(0)}; exit(1) }'"
       end
     end
   end
