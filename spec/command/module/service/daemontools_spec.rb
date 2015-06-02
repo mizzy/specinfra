d33f00d3b249f4bf9ca021143460aa11f0e2492c
Simon Detheridge <simon@widgit.com>
2015-06-02 14:40:57 +0100

    Support daemontools service dir in /etc
    Ubuntu puts the service dir in /etc/service, so fall back to this directory if /service does not exist

--- spec/command/module/service/daemontools_spec.rb
+++ spec/command/module/service/daemontools_spec.rb
@@ -5,14 +5,14 @@ describe Specinfra::Command::Module::Service::Daemontools do
     extend Specinfra::Command::Module::Service::Daemontools
   end
   let(:klass) { Specinfra::Command::Module::Service::Daemontools::Test }
-  it { expect(klass.check_is_enabled_under_daemontools('httpd')).to eq "test -L /service/httpd && test -f /service/httpd/run" }
-  it { expect(klass.check_is_running_under_daemontools('httpd')).to eq "svstat /service/httpd | grep -E 'up \\(pid [0-9]+\\)'" }
-  it { expect(klass.enable_under_daemontools('httpd', '/tmp/service/httpd')).to eq 'ln -snf /tmp/service/httpd /service/httpd' }
-  it { expect(klass.disable_under_daemontools('httpd')).to eq '( cd /service/httpd && rm -f /service/httpd && svc -dx . log )' }
-  it { expect(klass.start_under_daemontools('httpd')).to   eq 'svc -u /service/httpd' }
-  it { expect(klass.stop_under_daemontools('httpd')).to    eq 'svc -d /service/httpd' }
-  it { expect(klass.restart_under_daemontools('httpd')).to eq 'svc -t /service/httpd' }
-  it { expect(klass.reload_under_daemontools('httpd')).to  eq 'svc -h /service/httpd' }
+  it { expect(klass.check_is_enabled_under_daemontools('httpd')).to eq "test -L $([ -d /service ] && echo /service || echo /etc/service)/httpd && test -f $([ -d /service ] && echo /service || echo /etc/service)/httpd/run" }
+  it { expect(klass.check_is_running_under_daemontools('httpd')).to eq "svstat $([ -d /service ] && echo /service || echo /etc/service)/httpd | grep -E 'up \\(pid [0-9]+\\)'" }
+  it { expect(klass.enable_under_daemontools('httpd', '/tmp/service/httpd')).to eq 'ln -snf /tmp/service/httpd $([ -d /service ] && echo /service || echo /etc/service)/httpd' }
+  it { expect(klass.disable_under_daemontools('httpd')).to eq '( cd $([ -d /service ] && echo /service || echo /etc/service)/httpd && rm -f $([ -d /service ] && echo /service || echo /etc/service)/httpd && svc -dx . log )' }
+  it { expect(klass.start_under_daemontools('httpd')).to   eq 'svc -u $([ -d /service ] && echo /service || echo /etc/service)/httpd' }
+  it { expect(klass.stop_under_daemontools('httpd')).to    eq 'svc -d $([ -d /service ] && echo /service || echo /etc/service)/httpd' }
+  it { expect(klass.restart_under_daemontools('httpd')).to eq 'svc -t $([ -d /service ] && echo /service || echo /etc/service)/httpd' }
+  it { expect(klass.reload_under_daemontools('httpd')).to  eq 'svc -h $([ -d /service ] && echo /service || echo /etc/service)/httpd' }
 end
 
 
