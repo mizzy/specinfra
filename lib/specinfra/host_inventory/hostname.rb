c9c45d9bdb2469fcff1d3c60afcfbc19a49f1f21
Ryota Arai <ryota.arai@gmail.com>
2015-06-23 16:11:48 +0900

    domain, fqdn and hostname inventory value returns nil if it fails.

--- lib/specinfra/host_inventory/hostname.rb
+++ lib/specinfra/host_inventory/hostname.rb
@@ -3,7 +3,13 @@ module Specinfra
     class Hostname < Base
       def get
         cmd = backend.command.get(:get_inventory_hostname)
-        backend.run_command(cmd).stdout.strip
+        result = backend.run_command(cmd)
+
+        if result.exit_status == 0
+          result.stdout.strip
+        else
+          nil
+        end
       end
     end
   end
