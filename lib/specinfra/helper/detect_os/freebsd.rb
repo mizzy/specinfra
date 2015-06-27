4a7e5e0c69c6e95f30f91dc1eb979ba31c9c4b08
Urs Roesch <github@bun.ch>
2015-06-27 21:53:09 +0200

    detect_os/freebsd: Make release check more universally applicable

--- lib/specinfra/helper/detect_os/freebsd.rb
+++ lib/specinfra/helper/detect_os/freebsd.rb
@@ -1,8 +1,8 @@
 class Specinfra::Helper::DetectOs::Freebsd < Specinfra::Helper::DetectOs
   def detect
     if ( uname = run_command('uname -sr').stdout ) && uname =~ /FreeBSD/i
-      if uname =~ /10./
-        { :family => 'freebsd', :release => 10 }
+      if uname =~ /(\d+)\./
+        { :family => 'freebsd', :release => $1 }
       else
         { :family => 'freebsd', :release => nil }
       end
