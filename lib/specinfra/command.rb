c3c0d54983b7af4611eb117e0b481fbb2801fa37
Yuuitiro Matsumoto <yuuitiro@vcnet.toyama.toyama.jp>
2015-07-05 08:34:24 +0900

    Old FreeBSD can use have_home_directory and have_login_shell
    FreeBSD old version don't have 'getent'.
    It was imported into FreeBSD 7.0.
    
    * check_has_home_directory
    * check_has_login_shell
    * get_home_directory
    * get_login_shell
    
    Use 'pw' instead of 'getent'
    Test at FreeBSD 4.11, 8.4 and 7,3.

--- lib/specinfra/command.rb
+++ lib/specinfra/command.rb
@@ -210,10 +210,15 @@ require 'specinfra/command/freebsd/base/package'
 require 'specinfra/command/freebsd/base/port'
 require 'specinfra/command/freebsd/base/service'
 require 'specinfra/command/freebsd/base/routing_table'
+require 'specinfra/command/freebsd/base/user'
 
 # FreeBSD V10 (inherit FreeBSD)
 require 'specinfra/command/freebsd/v10'
 require 'specinfra/command/freebsd/v10/package'
+
+# FreeBSD V10 (inherit FreeBSD)
+require 'specinfra/command/freebsd/v6'
+require 'specinfra/command/freebsd/v6/user'
 
 # OpenBSD (inherit Base)
 require 'specinfra/command/openbsd'
