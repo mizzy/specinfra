d1cf29200652337306e8d013583ba76052987b71
Urs Roesch <github@bun.ch>
2015-07-10 23:00:25 +0200

    host_inventory/virtualization: Adding KVM plus unit test for kvm & docker

--- lib/specinfra/host_inventory/virtualization.rb
+++ lib/specinfra/host_inventory/virtualization.rb
@@ -5,6 +5,7 @@ module Specinfra
         res = {}
         if backend.run_command('ls /.dockerinit').success?
           res[:system] = 'docker'
+          return res
         end
 
         if backend.run_command('ls /usr/sbin/dmidecode').success?
@@ -19,6 +20,8 @@ module Specinfra
               if ret.stdout =~ /Product Name: VirtualBox/
                 res[:system] = 'vbox'
               end
+            when /Product Name: KVM/
+              res[:system] = 'kvm'
             when /Product Name: OpenStack/
               res[:system] = 'openstack'
             else
