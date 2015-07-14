7dd145c13d7eea0cc0f0c7949a7ad8606ec13007
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-07-14 15:58:36 +0900

    Add workaround for asterisk for ss command
    ss command displays "*" (asterisk) instead of "0.0.0.0".
    
    But serverspec validates IP address by `valid_ip_address?` method:
      https://github.com/serverspec/serverspec/blob/master/lib/serverspec/type/port.rb$

--- lib/specinfra/command/module/ss.rb
+++ lib/specinfra/command/module/ss.rb
@@ -4,9 +4,23 @@ module Specinfra
       module Ss
         def check_is_listening(port, options={})
           pattern = ":#{port} "
-          pattern = " #{options[:local_address]}#{pattern}" if options[:local_address]
+          pattern = " #{inaddr_any_to_asterisk(options[:local_address])}#{pattern}" if options[:local_address]
           pattern = "^#{options[:protocol]} .*#{pattern}" if options[:protocol]
           "ss -tunl | grep -- #{escape(pattern)}"
+        end
+
+        private
+
+        # WORKAROUND:
+        #   ss displays "*" instead of "0.0.0.0".
+        #   But serverspec validates IP address by `valid_ip_address?` method:
+        #     https://github.com/serverspec/serverspec/blob/master/lib/serverspec/type/port.rb
+        def inaddr_any_to_asterisk(local_address)
+          if local_address == '0.0.0.0'
+            '\*'
+          else
+            local_address
+          end
         end
       end
     end
