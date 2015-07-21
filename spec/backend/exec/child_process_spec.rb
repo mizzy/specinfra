ca6c8c59a85adbeb8bee81c125fab4fb89a54124
Gosuke Miyashita <gosukenator@gmail.com>
2015-07-21 22:18:17 +0900

    Add specs related to exec backend

---
+++ spec/backend/exec/child_process_spec.rb
@@ -0,0 +1,18 @@
+# Ref: https://github.com/sorah/infra_operator/blob/6259aada3fdd9d4bed5759115d39bd1df25a81f2/spec/backends/exec_spec.rb#L36
+
+require 'spec_helper'
+
+context "when executed process launches child process like a daemon, and the daemon doesn't close stdout,err" do
+  subject(:result) { Specinfra::Runner.run_command("ruby -e 'pid = fork { sleep 10; puts :bye }; Process.detach(pid); puts pid'") }
+
+  it "doesn't block" do
+    a = Time.now
+    result # exec
+    b = Time.now
+    expect((b-a) < 3).to be_truthy
+
+    expect(result.stderr).to be_empty
+    expect(result.stdout.chomp).to match(/\A\d+\z/)
+    Process.kill :TERM, result.stdout.chomp.to_i
+  end
+end
---
+++ spec/backend/exec/read_noblock_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+def command(cmd)
+  Specinfra::Runner.run_command(cmd)
+end
+
+shared_examples "IO checks" do
+    let (:generator) { "seq 1 #{max}" }
+    let (:expected) { (1..max).map { |x| x.to_s }.join("\n") << "\n" }
+
+    it "stdout only" do
+        out = command(generator).stdout
+        expect(out).to eq expected
+    end
+
+    it "stderr only" do
+        out = command("#{generator} >&2" ).stderr
+        expect(out).to eq expected
+    end
+
+    it "stdout then stderr" do
+        cmd = command("#{generator}; #{generator} >&2" )
+        expect(cmd.stdout).to eq expected
+        expect(cmd.stderr).to eq expected
+    end
+
+    it "stderr then stdout" do
+        cmd = command("#{generator} >&2; #{generator}" )
+        expect(cmd.stdout).to eq expected
+        expect(cmd.stderr).to eq expected
+    end
+
+    it "stdout and stderr" do
+        cmd = command("(#{generator} &); #{generator} >&2; sleep 2" )
+        expect(cmd.stdout).to eq expected
+        expect(cmd.stderr).to eq expected
+    end
+end
+
+describe "buffer overflow problem" do
+    context "with small output amount" do
+        let (:max) { 10 }
+        include_examples "IO checks"
+    end
+
+    context "with huge output amount" do
+        let (:max) { 999999 }
+        include_examples "IO checks"
+    end
+end
