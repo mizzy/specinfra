c8850ca5b2f470d15b8e7764733a1df539b11702
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-07-30 12:31:26 +0900

    Raise error if unknown protocol (and add specs)
    Now when protocol is set to 'tcpp', command_options returns "-tunl".
    But if it's unknown protocol, it's better to return error.

--- lib/specinfra/command/module/ss.rb
+++ lib/specinfra/command/module/ss.rb
@@ -23,13 +23,14 @@ module Specinfra
         end
 
         def command_options(protocol)
-          case protocol
+          case protocol.to_s
           when 'tcp'  then "-tnl4"
           when 'tcp6' then "-tnl6"
           when 'udp'  then "-unl4"
           when 'udp6' then "-unl6"
+          when ''     then "-tunl"
           else
-            "-tunl"
+            raise ArgumentError, "Unknown protocol [#{protocol}]"
           end
         end
       end
