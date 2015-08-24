4b6b1f24fd786cbcd248c0844a22e8fb5d4f73a1
Urs Roesch <github@bun.ch>
2015-08-24 02:05:43 +0200

    interface: Adding darwin support
     - get_link_state: Emulating Linux taking administratively down and physical layer into account
     - others: verbatim copy from freebsd

---
+++ spec/command/darwin/interface_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+property[:os] = nil
+set :os, :family => 'darwin'
+
+
+describe get_command(:check_interface_exists, 'en0') do
+  it { should eq "ifconfig en0" }
+end
+
+describe get_command(:check_interface_has_ipv6_address, 'en0', '2001:0db8:bd05:01d2:288a:1fc0:0001:10ee') do
+  it { should eq "ifconfig en0 inet6 | grep 'inet6 2001:0db8:bd05:01d2:288a:1fc0:0001:10ee '" }
+end
+
+describe get_command(:check_interface_has_ipv6_address, 'en0', '2001:0db8:bd05:01d2:288a:1fc0:0001:10ee/64') do
+  it { should eq "ifconfig en0 inet6 | grep 'inet6 2001:0db8:bd05:01d2:288a:1fc0:0001:10ee prefixlen 64'" }
+end
+
+describe get_command(:check_interface_has_ipv6_address, 'en0', 'fe80::5054:ff:fe01:10ee/64') do
+  it { should eq "ifconfig en0 inet6 | grep 'inet6 fe80::5054:ff:fe01:10ee%en0 prefixlen 64'" }
+end
+
+describe get_command(:check_interface_has_ipv6_address, 'en0', 'fe80::5054:ff:fe01:10ee') do
+  it { should eq "ifconfig en0 inet6 | grep 'inet6 fe80::5054:ff:fe01:10ee%en0 '" }
+end
+
+describe get_command(:check_interface_has_ipv4_address, 'en0', '192.168.0.123') do
+  it { should eq "ifconfig en0 inet | grep 'inet 192\\.168\\.0\\.123 '" }
+end
+
+describe get_command(:check_interface_has_ipv4_address, 'en0', '192.168.0.123/24') do
+  it { should eq "ifconfig en0 inet | grep 'inet 192\\.168\\.0\\.123 '" }
+end
+
+describe get_command(:get_interface_link_state, 'en0') do
+  it { should eq %Q{ifconfig -u en0 2>&1 | awk -v s=up '/status:/ && $2 != "active" { s="down" }; END {print s}'} }
+end
+
