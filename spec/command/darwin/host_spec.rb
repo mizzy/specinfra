e2c59ee50a429d55f128b0e0d1d683cfe6ee8518
Urs Roesch <github@bun.ch>
2015-08-25 00:25:05 +0200

    host: Adding darwin :ipv4_address and :ipv6_address
     - is_resolvable: Using dig instead of nslookup; simplyfies query and adds ipv6
     - get_*address: Using dscacheutil as getent is not available.

---
+++ spec/command/darwin/host_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+property[:os] = nil
+set :os, :family => 'darwin'
+
+describe get_command(:check_host_is_resolvable, 'pink.unicorn.com', 'dns') do 
+  it { should eq "dig +short +time=1 -q pink.unicorn.com a pink.unicorn.com aaaa | grep -qie '^[0-9a-f:.]*$'" } 
+end
+
+describe get_command(:check_host_is_resolvable, 'pink.unicorn.com', 'hosts') do 
+  it { should eq "sed 's/#.*$//' /etc/hosts | grep -w -- pink.unicorn.com" }
+end 
+
+describe get_command(:check_host_is_resolvable, 'pink.unicorn.com', nil) do 
+  it { should eq "dscacheutil -q host -a name pink.unicorn.com | grep -q '_address:'" } 
+end
+
+describe get_command(:check_host_is_reachable, 'pink.unicorn.com', nil, 'tcp', 10) do 
+  it { should eq "ping -t 10 -c 2 -n pink.unicorn.com"} 
+end
+
+describe get_command(:check_host_is_reachable, 'pink.unicorn.com', 53, 'udp', 66) do 
+  it { should eq "nc -vvvvzu pink.unicorn.com 53 -w 66" } 
+end
+
+describe get_command(:get_host_ipaddress, 'pink.unicorn.com') do 
+  it do should eq "dscacheutil -q host -a name pink.unicorn.com | " + 
+    "awk '/^ipv6_/{ ip = $2 }; /^$/{ exit }; /^ip_/{ ip = $2; exit}; END{ print ip }'"
+  end 
+end
+
+describe get_command(:get_host_ipv4_address, 'pink.unicorn.com') do 
+  it { should eq "dscacheutil -q host -a name pink.unicorn.com | awk '/^ip_/{ print $2; exit }'" }
+end 
+
+describe get_command(:get_host_ipv6_address, 'pink.unicorn.com') do 
+  it { should eq "dscacheutil -q host -a name pink.unicorn.com | awk '/^ipv6_/{ ip = $2 } END{ print ip }'" }
+end 
