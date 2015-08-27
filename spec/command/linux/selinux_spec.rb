54b1620acf77b6c72cf0eb354100a06cfe7e561d
Urs Roesch <github@bun.ch>
2015-08-27 02:33:07 +0200

    selinux: optional checking for policy when mode is permissive or enforcing

--- spec/command/linux/selinux_spec.rb
+++ spec/command/linux/selinux_spec.rb
@@ -4,5 +4,24 @@ property[:os] = nil
 set :os, :family => 'linux'
 
 describe get_command(:check_selinux_has_mode, 'disabled') do
-  it { should eq 'test ! -f /etc/selinux/config || (getenforce | grep -i -- disabled && grep -i -- ^SELINUX=disabled$ /etc/selinux/config)' }
+  it do 
+    should eq %Q{test ! -f /etc/selinux/config || (} +
+              %Q{getenforce | grep -i -- disabled} +
+              %Q{ && grep -iE -- '^\\s*SELINUX=disabled\\>' /etc/selinux/config)}
+  end 
+end
+
+describe get_command(:check_selinux_has_mode, 'permissive', nil) do
+  it do 
+    should eq %Q{getenforce | grep -i -- permissive} +
+              %Q{ && grep -iE -- '^\\s*SELINUX=permissive\\>' /etc/selinux/config}
+  end 
+end
+
+describe get_command(:check_selinux_has_mode, 'enforcing', 'targeted') do
+  it do 
+    should eq %Q{getenforce | grep -i -- enforcing} +
+              %Q{ && grep -iE -- '^\\s*SELINUX=enforcing\\>' /etc/selinux/config} +
+              %Q{ && grep -iE -- '^\\s*SELINUXTYPE=targeted\\>' /etc/selinux/config}
+  end 
 end
