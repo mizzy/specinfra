5eb2e59133a984a5b6cdf9c6ee942afbcc1ec253
Urs Roesch <github@bun.ch>
2015-09-01 01:26:00 +0200

    [WIP] host_inventory['network'] parser parsing interfaces and routes via ip command
    Summary: An inventory parser for Linux using a few ip commands to get the most
    interesting parts of the network information available. It is loosely based on
    the output of the Chef's Ohai but contains more information and treats IPv4 and
    IPv6 equally.
    
    Output structure:
    {
      :default_inet_interface  => "<if-name>",
      :default_inet6_interface => "<if-name>",
      :interfaces =>
        {
          "<device>" =>
            {
              :flags         => [<flag>, ..],
              :encapsulation => "<encapsulation>",
              :mtu           => "<mtu>",
              :qdisc         => "<qdisc>",
              :state         => "<state>"
              :addresses =>
                {
                  "<mac-address | ip-address>" =>
                    {
                      :family        =>  "<family>",
                      :prefixlen     =>  "<prefixlen>",
                      :scope         =>  "<scope>",
                      :flags         =>  [<flag>, ..],
                      :label         =>  "<label>",
                      :valid_lft     =>  "<valid_lft (ipv6)>",
                      :preferred_lft =>  "<preferred_lft (ipv6)>"
                    },
                   ...
                },
                ...
            },
            ...
        },
      :routes=>
        [
          {
            :advmss       => "advmss",
            :destination  => "<destination>",
            :device       => "<device>",
            :family       => "<family>",
            :hoplimit     => "<hoplimit>",
            :metric       => "<metric>",
            :mtu          => "<mtu>",
            :type         => "<type>"
          },
          ...
        ]
    }

--- lib/specinfra/command/linux/base/inventory.rb
+++ lib/specinfra/command/linux/base/inventory.rb
@@ -31,6 +31,18 @@ class Specinfra::Command::Linux::Base::Inventory < Specinfra::Command::Base::Inv
     def get_block_device
       block_device_dirs = '/sys/block/*/{size,removable,device/{model,rev,state,timeout,vendor},queue/rotational}'
       "for f in $(ls #{block_device_dirs}); do echo -e \"${f}\t$(cat ${f})\"; done"
+    end 
+
+    def get_interfaces
+      'ip addr show'
+    end
+
+    def get_ipv4_routing_table
+      'ip -4 route show' 
+    end
+
+    def get_ipv6_routing_table
+      'ip -6 route show' 
     end
 
     def get_system_product_name
--- lib/specinfra/host_inventory.rb
+++ lib/specinfra/host_inventory.rb
@@ -13,6 +13,7 @@ module Specinfra
       virtualization
       kernel
       block_device
+      network
     }
 
     include Enumerable
