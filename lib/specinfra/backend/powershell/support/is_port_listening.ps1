cff089ad2f35d1a068d5523e66e687f18cab6474
Craig Meinschein <cmeinsch@gmail.com>
2015-09-20 16:22:41 -0700

    Check localhost for listening ports on Windows.

--- lib/specinfra/backend/powershell/support/is_port_listening.ps1
+++ lib/specinfra/backend/powershell/support/is_port_listening.ps1
@@ -4,6 +4,7 @@ function IsPortListening
   $netstatOutput = netstat -an | Out-String
   $networkIPs = (Get-WmiObject Win32_NetworkAdapterConfiguration | ? {$_.IPEnabled}) | %{ $_.IPAddress[0] }
   [array] $networkIPs += "0.0.0.0"
+  [array] $networkIPs += "127.0.0.1"
   foreach ($ipaddress in $networkIPs)
   {
     $matchExpression = ("$ipaddress" + ":" + $portNumber)
