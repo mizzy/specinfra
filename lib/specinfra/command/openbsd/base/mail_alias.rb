22e3e287386c696cada6a3d3123db8a62c18729c
Urs Roesch <github@bun.ch>
2015-10-10 21:33:40 +0200

    mail_alias: Adjusting openbsd to behave the same as base

--- lib/specinfra/command/openbsd/base/mail_alias.rb
+++ lib/specinfra/command/openbsd/base/mail_alias.rb
@@ -1,7 +1,8 @@
 class Specinfra::Command::Openbsd::Base::MailAlias < Specinfra::Command::Base::MailAlias
   class << self
     def check_is_aliased_to(recipient, target)
-      "egrep '^#{escape(recipient)}:.*#{escape(target)}' /etc/mail/aliases"
+      target = %Q{([[:space:]]|,)["']?#{escape(target)}["']?(,|$)}
+      "egrep ^#{escape(recipient)}:.*#{escape(target)} /etc/mail/aliases"
     end
   end
 end
