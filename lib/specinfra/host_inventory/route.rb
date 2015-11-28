afb85fa6d15479c89f3d7bad65df8b76ac40ca16
Urs Roesch <github@bun.ch>
2015-11-28 09:47:39 +0100

    wip

--- lib/specinfra/host_inventory/route.rb
+++ lib/specinfra/host_inventory/route.rb
@@ -1,27 +1,23 @@
 module Specinfra
   class HostInventory
-    class Network < Base
+    class Route < Base
       def get
-        cmd = backend.command.get(:get_inventory_interfaces)
-        ret = backend.run_command(cmd)
-        if ret.exit_status == 0
-          ## currently linux only 
-          res = parse_interfaces(ret.stdout)
-          cmd = backend.command.get(:get_inventory_ipv4_routing_table)
-          ret = backend.run_command(cmd)
-          res.merge!(parse_ipv4_routing_table(ret.stdout))
-          cmd = backend.command.get(:get_inventory_ipv6_routing_table)
-          ret = backend.run_command(cmd)
-          res.merge!(parse_ipv6_routing_table(ret.stdout))
-          res 
-        else
+        begin 
+          route = {} 
+          cmd   = backend.command.get(:get_inventory_ipv4_routing_table)
+          ret   = backend.run_command(cmd)
+          inet  = parse_inet(ret.stdout)
+          cmd   = backend.command.get(:get_inventory_ipv6_routing_table)
+          ret   = backend.run_command(cmd)
+          inet6 = parse_inet6(ret.stdout)
+          { :inet => inet, :inet6 => inet6 }.delete_if { |k,v| v == nil }
+        rescue
           nil
         end
       end
 
-      def parse_ipv4_routing_table(ret) 
-         routes = {}                  
-         routes[:routes] = []                  
+      def parse_inet(ret) 
+         routes = []                  
          ret.each_line do |line| 
            if line =~ /(?:(unicast|nat|unreachable|prohibit|blackhole|throw)\s+)?
                        (default|(?:\d{1,3}\.){3}\d{1,3}(?:\/\d{1,2})?)\s+
@@ -32,7 +28,7 @@ module Specinfra
                        (?:src\s+(.*?)\s+)?
                        (?:metric\s+(\d{1,})\s+)?
                       /x
-              routes[:routes].push( 
+              routes.push( 
                 { 
                   :type        => ($1 ? $1 : 'unicast'),
                   :destination => $2, 
@@ -45,15 +41,13 @@ module Specinfra
                   :family      => 'inet' 
                 }.delete_if { |k,v| v == nil } 
               )
-              routes[:default_inet_interface] = $4 if $2 == 'default'
            end 
          end 
          routes 
       end 
 
-      def parse_ipv6_routing_table(ret) 
-         routes = {}                  
-         routes[:routes] = []                  
+      def parse_inet6(ret) 
+         routes = []                  
          ret.each_line do |line| 
            if line =~ /(?:(unicast|nat|unreachable|prohibit|blackhole|throw)\s+)?
                        (default|(?:[0-9a-fA-F:.]{2,39}(?:\/\d{1,3})?))\s+
@@ -66,7 +60,7 @@ module Specinfra
                        (?:advmss\s+(\d{1,})\s+)?
                        (?:hoplimit\s+(\d{1,})\s+)?
                       /x 
-              routes[:routes].push( 
+              routes.push( 
                 { 
                   :type        => ($1 ? $1 : 'unicast'),
                   :destination => $2, 
@@ -80,73 +74,10 @@ module Specinfra
                   :family      => 'inet6'
                 }.delete_if { |k,v| v == nil } 
               )
-              routes[:default_inet6_interface] = $4 if $2 == 'default'
            end 
          end 
          routes 
       end 
-
-      def parse_interfaces(ret)
-        interfaces = {}
-        @device    = nil
-        @address = nil
-        ret.each_line do |line|
-          if line =~ /^\d+:\s+([\w\d]+):?\s+
-                      <([A-Z\d_,]+)>\s+
-                      mtu\s+(\d+?)\s+
-                      qdisc\s+([\d\w]+?)\s+
-                      state\s+([A-Z]+)\s+
-                      (?:qlen\s+(\d+)\s+)?
-                      .*?
-                     /x 
-
-            @device= $1.to_s
-            interfaces[@device]        ||= {}
-            interfaces[@device][:mtu]    = $3
-            interfaces[@device][:qdisc]  = $4
-            interfaces[@device][:state]  = $5.to_s.downcase
-            interfaces[@device][:qlen]   = $6 if $6 != nil 
-            ## must process at end the result is skewed!
-            interfaces[@device][:flags]  = $2.to_s.split(',') 
-
-          elsif line =~ /^\s+link\/(\w+?)\s+(?:([A-Fa-f0-9:]+)\s+brd\s+([A-Fa-f0-9:]+))?/x 
-            interfaces[@device][:encapsulation]  = ($1 == 'ether' ? 'ethernet' : $1.to_s)
-            interfaces[@device][:addresses] ||= {}
-            interfaces[@device][:addresses][$2.to_s.upcase] = { :family => 'lladdr' } if $2 
-
-          elsif line =~ /^\s+
-                         (inet6?)\s+([a-fA-F\d:.]+)\/(\d+)\s+
-                         (?:brd\s+([\d.]+)\s+)?
-                         scope\s+(\w+?)\s+
-                         (?:((?:(?:temporary|permanent|dynamic|secondary|primary|tentative|deprecated)\s+)+))?
-                         (?:(#{@device}.*?)(?:\s+|$))?
-                        /x
-            @address = $2
-            interfaces[@device][:addresses] ||= {}
-            interfaces[@device][:addresses][@address] = {
-              :family    => $1,
-              :prefixlen => $3,
-              :broadcast => $4,
-              :scope     => $5,
-              :label     => $7,
-              ## must process at end the result is skewed!
-              :flags     => ($6.to_s.empty? ? nil : $6.split(/\s+/)) 
-            }.delete_if { |k,v| v == nil } 
-
-          elsif line =~ /^\s+
-                         valid_lft\s+([\w\d]+)\s+
-                         preferred_lft\s+([\w\d]+)\s+
-                        /x
-
-            interfaces[@device][:addresses][@address].merge!(
-              { :valid_lft     => $1,
-                :preferred_lft => $2
-              }.delete_if { |k,v| v == nil } 
-            )
-          end
-        end
-        { :interfaces => interfaces } 
-      end
     end
   end
 end
