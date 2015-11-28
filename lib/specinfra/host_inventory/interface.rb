9c2678bb80d13cecfd598f698acde67ef8048a02
Urs Roesch <github@bun.ch>
2015-11-28 09:17:31 +0100

    wip

--- lib/specinfra/host_inventory/interface.rb
+++ lib/specinfra/host_inventory/interface.rb
@@ -1,92 +1,17 @@
 module Specinfra
   class HostInventory
-    class Network < Base
+    class Interface < Base
       def get
-        cmd = backend.command.get(:get_inventory_interfaces)
-        ret = backend.run_command(cmd)
-        if ret.exit_status == 0
-          ## currently linux only 
-          res = parse_interfaces(ret.stdout)
-          cmd = backend.command.get(:get_inventory_ipv4_routing_table)
+        begin 
+          cmd = backend.command.get(:get_inventory_interfaces)
           ret = backend.run_command(cmd)
-          res.merge!(parse_ipv4_routing_table(ret.stdout))
-          cmd = backend.command.get(:get_inventory_ipv6_routing_table)
-          ret = backend.run_command(cmd)
-          res.merge!(parse_ipv6_routing_table(ret.stdout))
-          res 
-        else
+          parse(ret)
+        rescue
           nil
         end
       end
 
-      def parse_ipv4_routing_table(ret) 
-         routes = {}                  
-         routes[:routes] = []                  
-         ret.each_line do |line| 
-           if line =~ /(?:(unicast|nat|unreachable|prohibit|blackhole|throw)\s+)?
-                       (default|(?:\d{1,3}\.){3}\d{1,3}(?:\/\d{1,2})?)\s+
-                       (?:via\s+(.*?)\s+)?
-                       (?:dev\s+(.*?)\s+)?
-                       (?:proto\s+(.*?)\s+)?
-                       (?:scope\s+(.*?)\s+)?
-                       (?:src\s+(.*?)\s+)?
-                       (?:metric\s+(\d{1,})\s+)?
-                      /x
-              routes[:routes].push( 
-                { 
-                  :type        => ($1 ? $1 : 'unicast'),
-                  :destination => $2, 
-                  :next_hop    => $3,
-                  :device      => $4,
-                  :proto       => $5,
-                  :scope       => $6, 
-                  :source      => $7, 
-                  :metric      => $8, 
-                  :family      => 'inet' 
-                }.delete_if { |k,v| v == nil } 
-              )
-              routes[:default_inet_interface] = $4 if $2 == 'default'
-           end 
-         end 
-         routes 
-      end 
-
-      def parse_ipv6_routing_table(ret) 
-         routes = {}                  
-         routes[:routes] = []                  
-         ret.each_line do |line| 
-           if line =~ /(?:(unicast|nat|unreachable|prohibit|blackhole|throw)\s+)?
-                       (default|(?:[0-9a-fA-F:.]{2,39}(?:\/\d{1,3})?))\s+
-                       (?:via\s+(.*?)\s+)?
-                       (?:dev\s+(.*?)\s+)?
-                       (?:proto\s+(.*?)\s+)?
-                       (?:metric\s+(\d{1,})\s+)?
-                       (?:(?:error|expires)\s+.*?\s+)?
-                       (?:mtu\s+(\d{1,})\s+)?
-                       (?:advmss\s+(\d{1,})\s+)?
-                       (?:hoplimit\s+(\d{1,})\s+)?
-                      /x 
-              routes[:routes].push( 
-                { 
-                  :type        => ($1 ? $1 : 'unicast'),
-                  :destination => $2, 
-                  :next_hop    => $3,
-                  :device      => $4,
-                  :proto       => $5,
-                  :metric      => $6,
-                  :mtu         => $7,
-                  :advmss      => $8,
-                  :hoplimit    => $9,
-                  :family      => 'inet6'
-                }.delete_if { |k,v| v == nil } 
-              )
-              routes[:default_inet6_interface] = $4 if $2 == 'default'
-           end 
-         end 
-         routes 
-      end 
-
-      def parse_interfaces(ret)
+      def parse(ret)
         interfaces = {}
         @device    = nil
         @address = nil
@@ -145,7 +70,7 @@ module Specinfra
             )
           end
         end
-        { :interfaces => interfaces } 
+        interfaces
       end
     end
   end
