5368a65a36d697062c9765de22ea20a0bfd81fcd
Yuichiro NAITO <naito.yuichiro@gmail.com>
2016-01-26 12:11:26 +0900

    Fixed the way of changing encrypted password string.
    I had a advice of better implementation in FreeBSD workshop, Tokyo.

--- lib/specinfra/command/freebsd/base/user.rb
+++ lib/specinfra/command/freebsd/base/user.rb
@@ -9,7 +9,7 @@ class Specinfra::Command::Freebsd::Base::User < Specinfra::Command::Base::User
     end
 
     def get_minimum_days_between_password_change(user)
-      "echo 0"
+      'echo 0'
     end
 
     def get_maximum_days_between_password_change(user)
@@ -33,22 +33,21 @@ class Specinfra::Command::Freebsd::Base::User < Specinfra::Command::Base::User
     end
 
     def add(user, options)
-      if options[:password] then
-        command = ['echo',"\'#{escape(options[:password])}\'",'|','pw','user','add',escape(user),'-H','0']
-      else
-        command = ['pw','user','add',escape(user)]
-      end
+      command = ['pw', 'user', 'add', escape(user)]
       command << '-g' << escape(options[:gid])            if options[:gid]
       command << '-d' << escape(options[:home_directory]) if options[:home_directory]
       command << '-s' << escape(options[:shell])          if options[:shell]
       command << '-m' if options[:create_home]
       command << '-r' if options[:system_user]
       command << '-u' << escape(options[:uid])            if options[:uid]
+      if options[:password]
+        command.concat(['&&', 'chpass', '-p', "\'#{escape(options[:password])}\'", escape(user)])
+      end
       command.join(' ')
     end
 
     def update_encrypted_password(user, encrypted_password)
-        "echo \'#{escape(encrypted_password)}\' | sudo pw user mod #{escape(user)} -H 0"
+      "chpass -p \'#{escape(encrypted_password)}\' #{escape(user)}"
     end
 
     def get_encrypted_password(user)
