5e22ba0f63763302e1dfbf05f5b3fbfe6023d80a
Charles Hamel <charles.hamel@echo.com>
2016-02-25 11:49:19 -0600

    Added service_spec test for windows validation methods check_has_property check_is_running check_is_enabled

---
+++ spec/command/windows/service_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+property[:os] = nil
+set :os, :family => 'windows'
+RSpec.describe Specinfra::Command::Windows::Base::Service do
+    let(:service_name) { "Power" }
+    let(:property_map) {{"StartName"=>"Local System","State"=>"Running","StartMode"=>"Auto"}}
+    describe "#check_service_is_running" do
+        it "" do
+            command = get_command(:check_service_is_running, service_name)
+            expect(command.script).to eq("(FindService -name 'Power').State -eq 'Running'")
+        end
+    end
+    describe "#check_service_is_enabled" do
+        it "" do
+            command = get_command(:check_service_is_enabled, service_name)
+            expect(command.script).to eq ("(FindService -name 'Power').StartMode -eq 'Auto'")
+        end
+    end
+    describe "#has_property" do
+        it "includes (FindService -name 'Power').StartName -eq 'Local System" do
+            command = get_command(:check_service_has_property, service_name,property_map)
+            expect(command.script).to include ("(FindService -name 'Power').StartName -eq 'Local System'")
+        end
+        it "includes (FindService -name 'Power').State -eq 'Running" do
+            command = get_command(:check_service_has_property, service_name,property_map)
+            expect(command.script).to include ("(FindService -name 'Power').State -eq 'Running'")
+        end
+        it "includes (FindService -name 'Power').StartMode -eq 'Auto" do
+            command = get_command(:check_service_has_property, service_name,property_map)
+            expect(command.script).to include ("(FindService -name 'Power').StartMode -eq 'Auto'")
+        end
+    end
+end
