e786c8c7d14642bcf337eca8dd28735da7a4db01
Jere Julian <jere@arista.com>
2016-03-15 11:30:47 -0400

    Update EOS detection and add spec tests

--- lib/specinfra/helper/detect_os/eos.rb
+++ lib/specinfra/helper/detect_os/eos.rb
@@ -1,9 +1,9 @@
-class Specinfra::Helper::DetectOs::Redhat < Specinfra::Helper::DetectOs
+class Specinfra::Helper::DetectOs::Eos < Specinfra::Helper::DetectOs
   def detect
     # Arista Networks EOS
     if run_command('ls /etc/Eos-release').success?
       line = run_command('cat /etc/Eos-release').stdout
-      if line =~ /release (\d[\d.]*[A-Z]*)/
+      if line =~ /EOS (\d[\d.]*[A-Z]*)/
         release = $1
       end
       { :family => 'eos', :release => release }
