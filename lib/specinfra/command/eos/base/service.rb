ceae23bb3c813036f5114b09a20547ba3d4587d2
Jere Julian <jere@arista.com>
2016-03-15 14:42:43 -0400

    Add Arista EOS support based on Fedora

---
+++ lib/specinfra/command/eos/base/service.rb
@@ -0,0 +1,12 @@
+class Specinfra::Command::Eos::Base::Service < Specinfra::Command::Fedora::Base::Service
+#  class << self
+#    def create(os_info=nil)
+#      self
+#      #if (os_info || os)[:release].to_i < 15
+#      #  self
+#      #else
+#      #  Specinfra::Command::Fedora::V15::Service
+#      #end
+#    end
+#  end
+end
