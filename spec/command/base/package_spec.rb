03fb5c6b7c482b40b65a6ad83f54f6f38e38b77f
Matt Schuchard <grapheneowns@gmail.com>
2016-06-21 07:56:38 -0400

    fixes an issue where pip and gem check_is_installed gave false successes when similarly named packages were installed

--- spec/command/base/package_spec.rb
+++ spec/command/base/package_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 set :os, { :family => nil }
 
 describe get_command(:check_package_is_installed_by_gem, 'serverspec', '2.0.0') do
-  it { should eq 'gem list --local | grep -iw -- \\^serverspec | grep -w -- "[( ]2.0.0[,)]"' }
+  it { should eq 'gem list --local | grep -iw -- \\^serverspec\\  | grep -w -- "[( ]2.0.0[,)]"' }
 end
 
 describe get_command(:check_package_is_installed_by_td_agent_gem, 'fluent-plugin-forest', '0.3.0') do
-  it { should eq '/usr/sbin/td-agent-gem list --local | grep -iw -- \\^fluent-plugin-forest | grep -w -- "[( ]0.3.0[,)]"' }
+  it { should eq '/usr/sbin/td-agent-gem list --local | grep -iw -- \\^fluent-plugin-forest\\  | grep -w -- "[( ]0.3.0[,)]"' }
 end
 
 describe get_command(:check_package_is_installed_by_rvm, 'rbx', '2.4.1') do
