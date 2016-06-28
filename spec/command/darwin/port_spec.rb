3f73fc6467d69f494e2959a8d71eb6888716b0eb
James Fisher <fisher14@gmail.com>
2016-06-28 15:09:11 -0400

    fix ruby 1.8.7 syntax

--- spec/command/darwin/port_spec.rb
+++ spec/command/darwin/port_spec.rb
@@ -7,6 +7,6 @@ describe get_command(:check_port_is_listening, '80') do
   it { should eq 'lsof -nP -iTCP -sTCP:LISTEN | grep -- :80\ ' }
 end
 
-describe get_command(:check_port_is_listening, '80', protocol: 'udp') do
+describe get_command(:check_port_is_listening, '80', :protocol => 'udp') do
   it { should eq 'lsof -nP -iUDP | grep -- :80\ ' }
 end
