1fc8dc2ae17253df43d056427c985cd96904b136
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2016-09-18 22:32:01 +0200

    make the pkgng default for FreeBSD

---
+++ lib/specinfra/command/freebsd/v7/package.rb
@@ -0,0 +1,2 @@
+class Specinfra::Command::Freebsd::V7::Package <  Specinfra::Command::Freebsd::V6::Package
+end
---
+++ lib/specinfra/command/freebsd/v8.rb
@@ -0,0 +1,2 @@
+class Specinfra::Command::Freebsd::V8 < Specinfra::Command::Freebsd::Base
+end
