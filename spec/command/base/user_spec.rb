e9840cbf91c6abdaeb749abfbd13a814246c0292
Stefan Horning <stefan@hornings.de>
2016-10-17 13:43:24 +0200

    Added spec for check_user_has_authorized_key method for a start

--- spec/command/base/user_spec.rb
+++ spec/command/base/user_spec.rb
@@ -42,6 +42,12 @@ describe get_command(:check_user_has_login_shell, 'foo', '/bin/sh') do
   it { should eq "getent passwd foo | cut -f 7 -d ':' | grep -w -- /bin/sh" }
 end
 
+let(:key) { "ssh-rsa AAAAB3NzaC1yc2EAAA0zrqJ1DhN50nny08CvI8blPZTTXb0= test@test" }
+
+describe get_command(:check_user_has_authorized_key, 'foo', key) do
+  it { should eq "grep -w '#{key}' ~foo/.ssh/authorized_keys" }
+end
+
 describe get_command(:get_user_minimum_days_between_password_change, 'foo') do
   it { should eq "chage -l foo | sed -n 's/^Minimum.*: //p'" }
 end
