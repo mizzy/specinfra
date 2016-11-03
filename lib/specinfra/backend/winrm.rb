f5878477bba5ae6ef68da59b83f6aac22694952d
Nico Vanelslande <nicolas.vanelslande@gmail.com>
2016-11-03 14:34:41 +0000

    Keep backwards compatibility with winrm v1

--- lib/specinfra/backend/winrm.rb
+++ lib/specinfra/backend/winrm.rb
@@ -11,20 +11,34 @@ module Specinfra
         script = create_script(cmd)
         winrm = get_config(:winrm)
 
-        winrm.shell(:powershell) do |shell|
-          result = shell.run(script)
-          stdout = result.stdout.to_s
-          stderr = result.stderr.to_s
+        stdout, stderr = ''
+        exitcode = 0
 
-          result.exitcode = 1 if result.exitcode == 0 and !stderr.empty?
-
-          if @example
-            @example.metadata[:command] = script
-            @example.metadata[:stdout]  = stdout + stderr
+        if Gem.loaded_specs['winrm'].version < Gem::Version.create('2.0')
+          # Use winrm V1 API
+          result = winrm.powershell(script)
+          stdout, stderr = [:stdout, :stderr].map do |s|
+            result[:data].select {|item| item.key? s}.map {|item| item[s]}.join
           end
-
-          CommandResult.new :stdout => stdout, :stderr => stderr, :exit_status => result.exitcode
+          exitcode = result[:exitcode]
+        else
+          # Use winrm V2 API
+          winrm.shell(:powershell) do |shell|
+            result = shell.run(script)
+            stdout = result.stdout.to_s
+            stderr = result.stderr.to_s
+            exitcode = result.exitcode
+          end
         end
+
+        exitcode = 1 if exitcode == 0 and !stderr.empty?
+
+        if @example
+          @example.metadata[:command] = script
+          @example.metadata[:stdout]  = stdout + stderr
+        end
+
+        CommandResult.new :stdout => stdout, :stderr => stderr, :exit_status => exitcode
       end
     end
   end
