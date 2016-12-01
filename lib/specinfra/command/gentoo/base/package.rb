d68c3c59616ba43fb4b9798666b98c6ec92f8bcc
Hidenori MATSUKI <MATSUKI.Hidenori@gmail.com>
2016-12-01 20:10:33 +0900

    Implement some methods for `Gentoo::Base::Package` follows:
    * `get_version`
    * `install`
    * `remove`

--- lib/specinfra/command/gentoo/base/package.rb
+++ lib/specinfra/command/gentoo/base/package.rb
@@ -1,7 +1,24 @@
 class Specinfra::Command::Gentoo::Base::Package < Specinfra::Command::Linux::Base::Package
   class << self
     def check_is_installed(package, version=nil)
-      "eix #{escape(package)} --installed | grep -v \"No matches found.\""
+      "eix #{escape(package)} --installed | grep -v \"No matches found\""
+    end
+
+    def get_version(package, opts=nil)
+      "equery -q list #{package} | sed -e 's!^.*/?#{package}-!!'"
+    end
+
+    def install(package, version=nil, option='')
+      if version
+        full_package = "=#{package}-#{version}"
+      else
+        full_package = package
+      end
+      cmd = "emerge #{option} #{full_package}"
+    end
+
+    def remove(package, option='')
+      cmd = "emerge --unmerge #{option} #{package}"
     end
   end
 end
