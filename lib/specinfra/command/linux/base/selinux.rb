d9e5721f6afa72990d408474ede0bc21ef027ca5
canozokur <canozokur@noreply.users.github.com>
2016-12-14 00:01:54 +0300

    Change check mode shell command

--- lib/specinfra/command/linux/base/selinux.rb
+++ lib/specinfra/command/linux/base/selinux.rb
@@ -2,11 +2,11 @@ class Specinfra::Command::Linux::Base::Selinux < Specinfra::Command::Base::Selin
   class << self
     def check_has_mode(mode, policy = nil)
       cmd =  ""
-      cmd += "test ! -f /etc/selinux/config || (" if mode == "disabled"
-      cmd += "getenforce | grep -i -- #{escape(mode)}"
+      cmd += "test ! -f /etc/selinux/config || ( " if mode == "disabled"
+      cmd += "(getenforce | grep -i -- #{escape(mode)})"
+      cmd += " || (getenforce | grep -i -- #{escape('permissive')}) )" if mode == "disabled"
       cmd += %Q{ && grep -iE -- '^\\s*SELINUX=#{escape(mode)}\\>' /etc/selinux/config}
       cmd += %Q{ && grep -iE -- '^\\s*SELINUXTYPE=#{escape(policy)}\\>' /etc/selinux/config} if policy != nil
-      cmd += ")" if mode == "disabled"
       cmd
     end
   end
