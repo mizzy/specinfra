fac48d2e6a4f7bbb33a5dce2a5af909d44b31be5
Grzegorz Eliszewski <grzegorz@eliszewski.pl>
2017-03-24 16:07:50 +0100

    Fix detect opensuse and sles.

--- lib/specinfra/helper/detect_os/suse.rb
+++ lib/specinfra/helper/detect_os/suse.rb
@@ -1,9 +1,10 @@
 class Specinfra::Helper::DetectOs::Suse < Specinfra::Helper::DetectOs
   def detect
-    if run_command('ls /etc/SuSE-release').success?
-      line = run_command('cat /etc/SuSE-release').stdout
-      if line =~ /SUSE Linux Enterprise Server (\d+)/
-        release = $1
+    if run_command('ls /etc/os-release').success? and run_command('ls /etc/SuSe-release').success?
+      line = run_command('cat /etc/os-release').stdout
+      if line =~ /NAME=\"OpenSUSE"/
+        family = 'opensuse'
+      elsif line =~ /NAME=\"SLES"/
         family = 'sles'
       elsif line =~ /openSUSE (\d+\.\d+|\d+)/
         release = $1
