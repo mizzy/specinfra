51d2a6cdcdb87948b86b34d22f1b947b38f94078
Gosuke Miyashita <gosukenator@gmail.com>
2017-03-24 19:04:04 +0900

    Revert "Merge pull request #610 from es1o/master"
    This reverts commit ed2c16225d5162ed59a93a5219080b9ec3a20c8f, reversing
    changes made to af6aa37cfce5808934a6fa483c7453a5d7c75e64.
    
    Distributions other than suse also has /etc/os-release.
    So pr610 breaks os detection mechanism.

--- lib/specinfra/helper/detect_os/suse.rb
+++ lib/specinfra/helper/detect_os/suse.rb
@@ -1,14 +1,13 @@
 class Specinfra::Helper::DetectOs::Suse < Specinfra::Helper::DetectOs
   def detect
-    if run_command('ls /etc/os-release').success?
-      line = run_command('cat /etc/os-release').stdout
-      if line =~ /NAME=\"OpenSUSE"/
-        family = 'opensuse'
-      elsif line =~ /NAME=\"SLES"/
-        family = 'sles'
-      end
-      if line =~ /VERSION_ID=\"(\d+\.\d+|\d+)\"/
+    if run_command('ls /etc/SuSE-release').success?
+      line = run_command('cat /etc/SuSE-release').stdout
+      if line =~ /SUSE Linux Enterprise Server (\d+)/
         release = $1
+        family = 'sles'
+      elsif line =~ /openSUSE (\d+\.\d+|\d+)/
+        release = $1
+        family = 'opensuse'
       end
       { :family => family, :release => release }
     end
