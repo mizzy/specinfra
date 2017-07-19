5b69ffb8b71e82a0431f18e83f7f69cf54599ede
Matt Schuchard <grapheneowns@gmail.com>
2017-07-19 16:28:31 -0400

    Optional Facter for host_inventory
    Adds Facter as an optional source for additional host_inventory information.

---
+++ lib/specinfra/host_inventory/facter.rb
@@ -0,0 +1,17 @@
+module Specinfra
+  class HostInventory
+    class Facter < Base
+      require 'yaml'
+
+      def get
+        begin
+          ret = backend.run_command('facter --puppet --yaml')
+        rescue StandardError
+          nil
+        end
+
+        ret.exit_status.zero? ? YAML.load(ret.stdout) : nil
+      end
+    end
+  end
+end
