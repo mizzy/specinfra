4a887caee175fef2f440e6f9ab740e5011151dcd
Petr Řehoř <rx@rx.cz>
2017-08-22 13:01:24 +0200

    Allow to switch backends

--- lib/specinfra/helper/os.rb
+++ lib/specinfra/helper/os.rb
@@ -4,32 +4,8 @@ module Specinfra
   module Helper
     module Os
       def os
-        property[:os] = {} if ! property[:os]
-        if ! property[:os].include?(:family)
-          property[:os] = detect_os
-        end
-        property[:os]
-      end
-
-      private
-      def detect_os
-        return Specinfra.configuration.os if Specinfra.configuration.os
-
-        backend = Specinfra.configuration.backend
-        if backend == :cmd || backend == :winrm
-          return { :family => 'windows', :release => nil, :arch => nil }
-        end
-
-        Specinfra::Helper::DetectOs.subclasses.each do |c|
-          res = c.detect
-          if res
-            res[:arch] ||= Specinfra.backend.run_command('uname -m').stdout.strip
-            return res
-          end
-        end
-        raise NotImplementedError, "Specinfra failed os detection."
+        property[:os] = Specinfra.backend.os_info
       end
     end
   end
 end
-
