c796e651aa930ed2113d6031836082bbed34f074
Patrick F. Marques <patrickfmarques@gmail.com>
2017-10-31 14:30:06 +0000

    Update tests

--- spec/command/base/process_spec.rb
+++ spec/command/base/process_spec.rb
@@ -3,5 +3,5 @@ require 'spec_helper'
 set :os, { :family => nil }
 
 describe get_command(:count_process, 'foo') do
-  it { should eq 'ps aux | grep -w -- foo | grep -v grep | wc -l' }
+  it { should eq 'pidof foo | wc -w' }
 end
