9e08755c859c9ac4f06dc3e93c363b0fc98b06e5
nimDevOps <37848388+nimDevOps@users.noreply.github.com>
2018-08-16 13:48:22 -0400

    Removed double quotes, because they are not there

--- lib/specinfra/helper/detect_os/photon.rb
+++ lib/specinfra/helper/detect_os/photon.rb
@@ -4,7 +4,7 @@ class Specinfra::Helper::DetectOs::Photon < Specinfra::Helper::DetectOs
       line = run_command('cat /etc/os-release').stdout
       if line =~ /ID=photon/
         family = 'photon'
-        if line =~ /VERSION_ID=\"(\d+\.\d+|\d+)\"/
+        if line =~ /VERSION_ID=(\d+\.\d+|\d+)/
           release = $1
         end
         { :family => family, :release => release }
