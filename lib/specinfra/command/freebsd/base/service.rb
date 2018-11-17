6d54e29d9587b51ef261fe49b7634993c6ebe5fa
James Sanderson <zofrex@gmail.com>
2018-11-17 23:12:02 +0000

    Use built-in enabled check in FreeBSD's service command

--- lib/specinfra/command/freebsd/base/service.rb
+++ lib/specinfra/command/freebsd/base/service.rb
@@ -1,7 +1,7 @@
 class Specinfra::Command::Freebsd::Base::Service < Specinfra::Command::Base::Service
   class << self
     def check_is_enabled(service, level=3)
-      "service -e | grep -- #{escape(service)}"
+      "service #{escape(service)} enabled"
     end
     def check_is_running_under_init(service)
       "service #{escape(service)} status | grep -E 'as (pid [0-9]+)'"
