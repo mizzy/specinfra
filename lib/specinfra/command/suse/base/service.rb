264e7d876355ca4417a30585c12247069a82a94b
David Swan <david.swan@puppet.com>
2018-12-17 12:42:38 +0000

    Update so the 'check_is_enabled' works for all SLES versions In newer versions of SLES certain services have been moved from ‘chkconfig’ too ‘systemd’, thus preventing the current command from finding them. This change is meant to account for that.

--- lib/specinfra/command/suse/base/service.rb
+++ lib/specinfra/command/suse/base/service.rb
@@ -3,6 +3,14 @@ class Specinfra::Command::Suse::Base::Service < Specinfra::Command::Linux::Base:
     def check_is_enabled(service, level=3)
       "chkconfig --list #{escape(service)} | grep #{level}:on"
     end
+
+    def create(os_info=nil)
+      if (os_info || os)[:release].to_i < 12
+        self
+      else
+        Specinfra::Command::Sles::V12::Service
+      end
+    end
   end
 end
 
