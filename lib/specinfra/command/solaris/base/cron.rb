b494da5143f3c6078e90cb32b5ea9326531132e4
lomeroe <github@proxyman.com>
2019-01-16 16:17:50 -0600

    Add '%' to characters manually escaped in solaris cron check (#679)

--- lib/specinfra/command/solaris/base/cron.rb
+++ lib/specinfra/command/solaris/base/cron.rb
@@ -1,7 +1,7 @@
 class Specinfra::Command::Solaris::Base::Cron < Specinfra::Command::Base::Cron
   class << self
     def check_has_entry(user, entry)
-      entry_escaped = entry.gsub(/\*/, '\\*').gsub(/\[/, '\\[').gsub(/\]/, '\\]')
+      entry_escaped = entry.gsub(/\*/, '\\*').gsub(/\[/, '\\[').gsub(/\]/, '\\]').gsub(/\%/, '\\%')
       if user.nil?
         "crontab -l | grep -- #{escape(entry_escaped)}"
       else
