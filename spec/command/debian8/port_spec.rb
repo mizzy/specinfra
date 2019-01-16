46db575f1d74222c4f83a9e3bcb215e94069f15b
Matthias Baur <m.baur@syseleven.de>
2019-01-16 10:31:02 +0100

    Add support for ss > 4.14.0
    The output format of ss changed starting with version 4.14.0 due to this
    change:
    
    https://git.kernel.org/pub/scm/network/iproute2/iproute2.git/commit/?id=1267c0b924de0297510efd48f8f849af774be369
    
    This change ensure that specinfra works with both, older and newer,
    versions of ss.

--- spec/command/debian8/port_spec.rb
+++ spec/command/debian8/port_spec.rb
@@ -4,5 +4,5 @@ property[:os] = nil
 set :os, :family => 'debian', :release => '8'
 
 describe get_command(:check_port_is_listening, '80') do
-  it { should eq 'ss -tunl | grep -- :80\ ' }
+  it { should eq 'ss -tunl | grep -E -- :80\ ' }
 end
--- spec/command/debian9/port_spec.rb
+++ spec/command/debian9/port_spec.rb
@@ -4,6 +4,6 @@ property[:os] = nil
 set :os, :family => 'debian', :release => '9'
 
 describe get_command(:check_port_is_listening, '80') do
-  it { should eq 'ss -tunl | grep -- :80\ ' }
+  it { should eq 'ss -tunl | grep -E -- :80\ ' }
 end
 
