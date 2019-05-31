7f38d86c45ea6ede3f81558a2796a4dbd997f961
katsuyuki12 <50938848+katsuyuki12@users.noreply.github.com>
2019-05-31 18:38:09 +0900

    Update aix.rb

--- lib/specinfra/helper/detect_os/aix.rb
+++ lib/specinfra/helper/detect_os/aix.rb
@@ -3,9 +3,17 @@ class Specinfra::Helper::DetectOs::Aix < Specinfra::Helper::DetectOs
     if run_command('uname -s').stdout =~ /AIX/i
       line = run_command('uname -rvp').stdout
       if line =~ /(\d+)\s+(\d+)\s+(.*)/ then
-        { :family => 'aix', :release => "#{$2}.#{$1}", :arch => $3 }
+        oslevel = run_command('oslevel -s').stdout.chomp
+        edition = run_command('chedition -l').stdout.chomp
+        { :family => 'aix', :release => "#{$2}.#{$1}", :arch => $3, :oslevel => "#{oslevel}", :edition => "#{edition}" }
       else
-        { :family => 'aix', :release => nil, :arch => nil }
+        if run_command('ioslevel').success?
+          oslevel = run_command('ioslevel').stdout.chomp
+          edition = run_command('chedition -list').stdout.chomp
+          { :family => 'aix', :release => nil, :arch => 'vios', :oslevel => "#{oslevel}", :edition => "#{edition}" }
+        else
+          { :family => 'aix', :release => nil, :arch => nil, :oslevel => nil, :edition => nil }
+        end
       end
     end
   end
