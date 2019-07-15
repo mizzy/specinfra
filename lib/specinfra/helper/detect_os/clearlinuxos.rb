9eca20539b89b505d7f18cae15a4a4412bb68756
grothja <grothja@niaid.nih.gov>
2019-07-15 10:46:23 -0400

    Fix Clear Linux detection so it works w/ 1.8.7

--- lib/specinfra/helper/detect_os/clearlinuxos.rb
+++ lib/specinfra/helper/detect_os/clearlinuxos.rb
@@ -1,9 +1,7 @@
 class Specinfra::Helper::DetectOs::Clearlinuxos < Specinfra::Helper::DetectOs
   def detect
-    if (clearlinuxos_info = run_command('/usr/bin/swupd')) && clearlinuxos_info.success?
-      distro = 'clearlinuxos'
-      release = nil
-      { family: distro, release: release }
+    if run_command('/usr/bin/swupd').success?
+      { :family => 'nixos', :release => nil }
     end
   end
 end
