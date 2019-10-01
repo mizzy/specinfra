3f0ab4251ae659d3d01fc8b4297e489a2055d2a7
Mark Chappell <mchappel@redhat.com>
2019-10-01 14:12:52 +0200

    Wire in RHEL7 Service/Port/Host tests

--- lib/specinfra/command/redhat/base/port.rb
+++ lib/specinfra/command/redhat/base/port.rb
@@ -1,2 +1,11 @@
 class Specinfra::Command::Redhat::Base::Port < Specinfra::Command::Base::Port
+  class << self
+    def create(os_info=nil)
+      if (os_info || os)[:release].to_i < 7
+        self
+      else
+        Specinfra::Command::Redhat::V7::Port
+      end
+    end
+  end
 end
--- lib/specinfra/command/redhat/base/service.rb
+++ lib/specinfra/command/redhat/base/service.rb
@@ -1,2 +1,11 @@
 class Specinfra::Command::Redhat::Base::Service < Specinfra::Command::Linux::Base::Service
+  class << self
+    def create(os_info=nil)
+      if (os_info || os)[:release].to_i < 7
+        self
+      else
+        Specinfra::Command::Redhat::V7::Service
+      end
+    end
+  end
 end
