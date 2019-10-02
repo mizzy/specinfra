6038f67797a2fd6840684dd0fde97717f1090339
Gosuke Miyashita <gosukenator@gmail.com>
2019-10-03 07:55:24 +0900

    Fix for compatibility with amazon linux

--- lib/specinfra/command/redhat/base/port.rb
+++ lib/specinfra/command/redhat/base/port.rb
@@ -1,7 +1,9 @@
 class Specinfra::Command::Redhat::Base::Port < Specinfra::Command::Base::Port
   class << self
     def create(os_info=nil)
-      if (os_info || os)[:release].to_i < 7
+      release = (os_info || os)[:release].to_i
+      # Dirty hack for Amazon Linux
+      if release < 7 || release > 2000
         self
       else
         Specinfra::Command::Redhat::V7::Port
