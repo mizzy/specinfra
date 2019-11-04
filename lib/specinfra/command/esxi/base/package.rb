02c879ed88b6c22082cdae830fc9b60d4b2a5cae
volanja <volaaanja@gmail.com>
2019-11-04 23:58:02 +0900

    add check version to check_is_installed for Esxi::Base::Package

--- lib/specinfra/command/esxi/base/package.rb
+++ lib/specinfra/command/esxi/base/package.rb
@@ -1,7 +1,11 @@
 class Specinfra::Command::Esxi::Base::Package < Specinfra::Command::Linux::Base::Package
   class << self
     def check_is_installed(package, version=nil)
-      "esxcli software vib list | grep #{escape(package)}"
+      cmd = "esxcli software vib list | grep -w -- #{escape(package)}"
+      if version
+        cmd = "#{cmd} | grep -w -- #{escape(version)}"
+      end
+      cmd
     end
   end
 end
