f210b1cc86dcdee397d2661ee32a379082ff7b27
Takashi Kokubun <takashikkbn@gmail.com>
2019-12-30 00:52:57 -0800

    Fix a constant name inconsistent with its path

--- lib/specinfra/command/ubuntu/v18/port.rb
+++ lib/specinfra/command/ubuntu/v18/port.rb
@@ -1,4 +1,4 @@
-class Specinfra::Command::Ubuntu::Base::V18::Port < Specinfra::Command::Ubuntu::Base::Port
+class Specinfra::Command::Ubuntu::V18::Port < Specinfra::Command::Ubuntu::Base::Port
   class << self
     include Specinfra::Command::Module::Ss
   end
