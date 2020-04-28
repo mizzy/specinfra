fbe4158b3a396c95719a14e01982a833ad199bda
benpearce1 <ben.pearce@octopus.com>
2020-04-28 13:10:58 +1000

    Ensure the matched port is listening allow matching on ipv6 local

--- lib/specinfra/backend/powershell/support/is_port_listening.ps1
+++ lib/specinfra/backend/powershell/support/is_port_listening.ps1
@@ -5,9 +5,10 @@ function IsPortListening
   $networkIPs = (Get-WmiObject Win32_NetworkAdapterConfiguration | ? {$_.IPEnabled}) | %{ $_.IPAddress[0] }
   [array] $networkIPs += "0.0.0.0"
   [array] $networkIPs += "127.0.0.1"
+  [array] $networkIPs += "[::1]"
   foreach ($ipaddress in $networkIPs)
   {
-    $matchExpression = ("$ipaddress" + ":" + $portNumber)
+    $matchExpression = ("$ipaddress" + ":" + $portNumber + ".*LISTENING")
     if ($protocol) { $matchExpression = ($protocol.toUpper() + "\s+$matchExpression") }
     if ($netstatOutput -match $matchExpression) { return $true }
   }
