0283f79de65a9c32b1741e9dd00aafd9a0582c39
Brian Call <6537446+call@users.noreply.github.com>
2020-06-13 21:11:07 -0700

    Add get_size(file) for darwin

--- lib/specinfra/command/darwin/base/file.rb
+++ lib/specinfra/command/darwin/base/file.rb
@@ -34,6 +34,10 @@ class Specinfra::Command::Darwin::Base::File < Specinfra::Command::Base::File
     def get_mode(file)
       "stat -f%Lp #{escape(file)}"
     end
+    
+    def get_size(file)
+      "stat -f %z #{escape(file)}"
+    end
 
     def get_owner_user(file)
       "stat -f %Su #{escape(file)}"
