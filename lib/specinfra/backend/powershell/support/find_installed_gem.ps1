b1301d8c3bef65695d88fc0c52536d32ff406840
Kentaro Hayashi <kenhys@gmail.com>
2020-08-24 15:49:31 +0900

    Windows: use more accurate prefix search

--- lib/specinfra/backend/powershell/support/find_installed_gem.ps1
+++ lib/specinfra/backend/powershell/support/find_installed_gem.ps1
@@ -2,7 +2,7 @@ function FindInstalledGem
 {
   param($gemName, $gemVersion)
 
-  $nameVer = $(Invoke-Expression "gem list --local" | Select-String "$gemName").Line
+  $nameVer = $(Invoke-Expression "gem list --local" | Select-String "^$gemName").Line
   if ($nameVer.StartsWith($gemName)) {
     if ($gemVersion) {
       $versions = ($nameVer -split { $_ -eq "(" -or $_ -eq ")"})[1].split(" ")
